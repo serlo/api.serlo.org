b4cfdf2137011e686e8c99410a7f3c442a937031
Jonas Keinholz <git@inyono.dev>
2020-03-31 12:54:50 +0200

    add _set* for uuid types

--- __tests__/license.ts
+++ __tests__/license.ts
@@ -1,55 +1,10 @@
 import { gql } from 'apollo-server'
 
+import { Service } from '../src/graphql/schema/types'
 import { createTestClient } from './utils/test-client'
 
-test('by id', async () => {
-  const { cache, client } = createTestClient({ service: null })
-  await cache.set(
-    'de.serlo.org/api/license/1',
-    JSON.stringify({
-      id: 1,
-      instance: 'de',
-      default: true,
-      title: 'title',
-      url: 'url',
-      content: 'content',
-      agreement: 'agreement',
-      iconHref: 'iconHref',
-    })
-  )
-  const response = await client.query({
-    query: gql`
-      {
-        license(id: 1) {
-          id
-          instance
-          default
-          title
-          url
-          content
-          agreement
-          iconHref
-        }
-      }
-    `,
-  })
-  expect(response.errors).toBe(undefined)
-  expect(response.data).toEqual({
-    license: {
-      id: 1,
-      instance: 'de',
-      default: true,
-      title: 'title',
-      url: 'url',
-      content: 'content',
-      agreement: 'agreement',
-      iconHref: 'iconHref',
-    },
-  })
-})
-
-test('_setLicense (unauthenticated)', async () => {
-  const { client } = createTestClient({ service: null })
+test('_setLicense (forbidden)', async () => {
+  const { client } = createTestClient({ service: Service.Playground })
   const response = await client.mutate({
     mutation: gql`
       mutation {
@@ -66,11 +21,11 @@ test('_setLicense (unauthenticated)', async () => {
       }
     `,
   })
-  expect(response.errors?.[0].extensions?.code).toEqual('UNAUTHENTICATED')
+  expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
 })
 
 test('_setLicense (authenticated)', async () => {
-  const { client } = createTestClient({ service: 'serlo.org' })
+  const { client } = createTestClient({ service: Service.Serlo })
 
   let response = await client.mutate({
     mutation: gql`
