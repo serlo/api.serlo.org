aa6fc143f989f57c68d1fbbe4e602f8a97e9859e
Jonas Keinholz <git@inyono.dev>
2020-04-10 13:52:31 +0200

    remove health check route `/`, add changelog
    Close #6

---
+++ scripts/changelog.ts
@@ -0,0 +1,40 @@
+import { generateChangelog } from '@splish-me/changelog'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as util from 'util'
+
+const writeFile = util.promisify(fs.writeFile)
+
+exec()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
+async function exec(): Promise<void> {
+  const content = await generateChangelog([
+    {
+      tagName: 'v0.1.0',
+      name: '0.1.0',
+      date: '2020-04-10',
+      description: 'Initial release',
+    },
+    {
+      tagName: 'v0.1.1',
+      name: '0.1.1',
+      date: '2020-04-11',
+      added: ['Add health check route `/`.'],
+      fixed: ['Lazily create token for GraphQL playground.'],
+    },
+    {
+      breakingChanges: [
+        "Remove health check route `/`. Use Apollo's health check route `.well-known/apollo/server-health` instead.",
+      ],
+    },
+  ])
+
+  await writeFile(path.join(__dirname, '..', 'CHANGELOG.md'), content)
+}
