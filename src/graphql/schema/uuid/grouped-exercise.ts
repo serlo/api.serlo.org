0877c638eddaccc1eb8b251058d01e8ccde093d7
Jonas Keinholz <git@inyono.dev>
2020-04-14 16:17:43 +0200

    add exerciseGroup to GroupedExercise

--- src/graphql/schema/uuid/grouped-exercise.ts
+++ src/graphql/schema/uuid/grouped-exercise.ts
@@ -7,6 +7,7 @@ import {
   EntityPayload,
 } from './abstract-entity'
 import { ExerciseRevision, ExerciseRevisionPayload } from './exercise'
+import { ExerciseGroup } from './exercise-group'
 import { Solution } from './solution'
 
 export const groupedExerciseSchema = new Schema()
@@ -14,14 +15,17 @@ export const groupedExerciseSchema = new Schema()
 export class GroupedExercise extends Entity {
   public __typename = EntityType.GroupedExercise
   public solutionId: number | null
+  public parentId: number
 
   public constructor(payload: GroupedExercisePayload) {
     super(payload)
     this.solutionId = payload.solutionId
+    this.parentId = payload.parentId
   }
 }
 export interface GroupedExercisePayload extends EntityPayload {
   solutionId: number | null
+  parentId: number
 }
 groupedExerciseSchema.addResolver<
   GroupedExercise,
@@ -40,6 +44,22 @@ groupedExerciseSchema.addResolver<
     return new Solution(data)
   }
 )
+groupedExerciseSchema.addResolver<
+  GroupedExercise,
+  unknown,
+  Partial<ExerciseGroup>
+>(
+  'GroupedExercise',
+  'exerciseGroup',
+  async (entity, _args, { dataSources }, info) => {
+    const partialExerciseGroup = { id: entity.parentId }
+    if (requestsOnlyFields('ExerciseGroup', ['id'], info)) {
+      return partialExerciseGroup
+    }
+    const data = await dataSources.serlo.getUuid(partialExerciseGroup)
+    return new ExerciseGroup(data)
+  }
+)
 
 export class GroupedExerciseRevision extends ExerciseRevision {
   public __typename = EntityRevisionType.GroupedExerciseRevision
@@ -55,9 +75,11 @@ addEntityResolvers({
   EntityRevision: GroupedExerciseRevision,
   entityFields: `
     solution: Solution
+    exerciseGroup: ExerciseGroup!
   `,
   entityPayloadFields: `
     solutionId: Int
+    parentId: Int!
   `,
   entityRevisionFields: `
     content: String!
