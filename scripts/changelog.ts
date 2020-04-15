1df96a98b5f1453f0bf2ae0a35aa62e3b2c09b95
Jonas Keinholz <git@inyono.dev>
2020-04-15 04:39:22 +0200

    update changelog

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -30,13 +30,25 @@ async function exec(): Promise<void> {
       fixed: ['Lazily create token for GraphQL playground.'],
     },
     {
-      tagName: 'v.0.2.0',
+      tagName: 'v0.2.0',
       name: '0.2.0',
       date: '2020-04-13',
       breakingChanges: [
         "Remove health check route `/`. Use Apollo's health check route `.well-known/apollo/server-health` instead.",
       ],
       added: ['Add descriptions to the GraphQL schema.'],
+    },
+    {
+      tagName: 'v0.3.0',
+      name: '0.3.0',
+      date: '2020-04-15',
+      added: [
+        'Add entity types `Course` and `CoursePage`.',
+        'Add entity types `ExerciseGroup`, `GroupedExercise`, `Exercise`, and `Solution`.',
+        'Add entity type `Applet`.',
+        'Add entity type `Event`.',
+        'Add entity type `Video`.',
+      ],
     },
   ])
 
