7261948d35dd71bc2929e52660b0d653b5b011b0
Stephan Kulla <git.mail@kulla.me>
2020-07-14 15:36:15 +0200

    Move enviornment settings to .env file

--- jest.setup.ts
+++ jest.setup.ts
@@ -19,12 +19,6 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
  */
-import { env } from 'process'
-
 import { extendExpect } from './__tests__/_extend-jest'
 
 extendExpect()
-
-beforeEach(() => {
-  env.GOOGLE_API_KEY = ''
-})
