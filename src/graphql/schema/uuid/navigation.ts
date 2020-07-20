de861c7cd402594588f82a8fc4aa196b3db38e41
Jonas Keinholz <git@inyono.dev>
2020-07-21 00:19:24 +0200

    navigation: remove _setNavigation, fix tests

--- src/graphql/schema/uuid/navigation.ts
+++ src/graphql/schema/uuid/navigation.ts
@@ -19,10 +19,9 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
  */
-import { ForbiddenError, gql } from 'apollo-server'
+import { gql } from 'apollo-server'
 
 import { Instance } from '../instance'
-import { Service } from '../types'
 import { Schema } from '../utils'
 
 export const navigationSchema = new Schema()
@@ -51,36 +50,7 @@ navigationSchema.addTypeDef(gql`
   }
 `)
 
-/**
- * mutation _setNavigation
- */
-navigationSchema.addMutation<unknown, NavigationPayload, null>(
-  '_setNavigation',
-  async (_parent, payload, { dataSources, service }) => {
-    if (service !== Service.Serlo) {
-      throw new ForbiddenError(
-        `You do not have the permissions to set the navigation`
-      )
-    }
-    await dataSources.serlo.setNavigation(payload)
-  }
-)
 export interface NavigationPayload {
   data: string
   instance: Instance
-}
-navigationSchema.addTypeDef(gql`
-  extend type Mutation {
-    _setNavigation(data: String!, instance: Instance!): Boolean
-  }
-`)
-export function setNavigation(variables: NavigationPayload) {
-  return {
-    mutation: gql`
-      mutation setNavigation($data: String!, $instance: Instance!) {
-        _setNavigation(data: $data, instance: $instance)
-      }
-    `,
-    variables,
-  }
 }
