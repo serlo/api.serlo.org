9320e7f9a8e1adcde6fe0343ab3edb008a8c44b9
Jonas Keinholz <git@inyono.dev>
2020-07-24 04:47:52 +0200

    course-page: create unit tests

---
+++ __tests__/schema/uuid/course-page.ts
@@ -0,0 +1,198 @@
+/**
+ * This file is part of Serlo.org API
+ *
+ * Copyright (c) 2020 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2020 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
+ */
+import { gql } from 'apollo-server'
+
+import {
+  course,
+  coursePage,
+  coursePageRevision,
+  getCourseDataWithoutSubResolvers,
+  getCoursePageDataWithoutSubResolvers,
+  getCoursePageRevisionDataWithoutSubResolvers,
+} from '../../../__fixtures__'
+import { Service } from '../../../src/graphql/schema/types'
+import {
+  assertSuccessfulGraphQLQuery,
+  Client,
+  createTestClient,
+  createUuidHandler,
+} from '../../__utils__'
+
+let client: Client
+
+beforeEach(() => {
+  client = createTestClient({
+    service: Service.Playground,
+    user: null,
+  }).client
+})
+
+describe('CoursePage', () => {
+  beforeEach(() => {
+    global.server.use(createUuidHandler(coursePage))
+  })
+
+  test('by id', async () => {
+    await assertSuccessfulGraphQLQuery({
+      query: gql`
+        query coursePage($id: Int!) {
+          uuid(id: $id) {
+            __typename
+            ... on CoursePage {
+              id
+              trashed
+              alias
+              instance
+              date
+            }
+          }
+        }
+      `,
+      variables: coursePage,
+      data: {
+        uuid: getCoursePageDataWithoutSubResolvers(coursePage),
+      },
+      client,
+    })
+  })
+
+  test('by id (w/ currentRevision)', async () => {
+    global.server.use(createUuidHandler(coursePageRevision))
+    await assertSuccessfulGraphQLQuery({
+      query: gql`
+        query coursePage($id: Int!) {
+          uuid(id: $id) {
+            ... on CoursePage {
+              currentRevision {
+                __typename
+                id
+                trashed
+                date
+                title
+                content
+                changes
+              }
+            }
+          }
+        }
+      `,
+      variables: coursePage,
+      data: {
+        uuid: {
+          currentRevision: getCoursePageRevisionDataWithoutSubResolvers(
+            coursePageRevision
+          ),
+        },
+      },
+      client,
+    })
+  })
+
+  test('by id (w/ course)', async () => {
+    global.server.use(createUuidHandler(course))
+    await assertSuccessfulGraphQLQuery({
+      query: gql`
+        query coursePage($id: Int!) {
+          uuid(id: $id) {
+            ... on CoursePage {
+              course {
+                __typename
+                id
+                trashed
+                alias
+                instance
+                date
+              }
+            }
+          }
+        }
+      `,
+      variables: coursePage,
+      data: {
+        uuid: {
+          course: getCourseDataWithoutSubResolvers(course),
+        },
+      },
+      client,
+    })
+  })
+})
+
+describe('CoursePageRevision', () => {
+  beforeEach(() => {
+    global.server.use(createUuidHandler(coursePageRevision))
+  })
+
+  test('by id', async () => {
+    await assertSuccessfulGraphQLQuery({
+      query: gql`
+        query coursePageRevision($id: Int!) {
+          uuid(id: $id) {
+            __typename
+            ... on CoursePageRevision {
+              id
+              trashed
+              date
+              title
+              content
+              changes
+            }
+          }
+        }
+      `,
+      variables: coursePageRevision,
+      data: {
+        uuid: getCoursePageRevisionDataWithoutSubResolvers(coursePageRevision),
+      },
+      client,
+    })
+  })
+
+  test('by id (w/ coursePage)', async () => {
+    global.server.use(createUuidHandler(coursePage))
+    await assertSuccessfulGraphQLQuery({
+      query: gql`
+        query coursePageRevision($id: Int!) {
+          uuid(id: $id) {
+            ... on CoursePageRevision {
+              coursePage {
+                __typename
+                id
+                trashed
+                alias
+                instance
+                date
+              }
+            }
+          }
+        }
+      `,
+      variables: coursePageRevision,
+      data: {
+        uuid: {
+          coursePage: getCoursePageDataWithoutSubResolvers(coursePage),
+        },
+      },
+      client,
+    })
+  })
+})
