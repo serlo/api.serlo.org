7b7f012e9dc247be9cf0455952088e6f2c3ba70d
Jonas Keinholz <git@inyono.dev>
2020-11-30 21:11:22 +0100

    cacheable-data-source: use in-memory lock

--- __tests__/data-sources/cacheable-data-source.ts
+++ __tests__/data-sources/cacheable-data-source.ts
@@ -69,12 +69,8 @@ describe('getCacheValue', () => {
       test('returns cached value without background update', async () => {
         await initCache({ current: 0 })
         await waitFor(10)
-        await execInParallelAndWaitForBackgroundTasks(async () => {
-          expect(await dataSource.getContent({ current: 1 })).toEqual(0)
-        })
-        await execInParallelAndWaitForBackgroundTasks(async () => {
-          expect(await dataSource.getContent({ current: 2 })).toEqual(0)
-        })
+        expect(await dataSource.getContent({ current: 1 })).toEqual(0)
+        expect(await dataSource.getContent({ current: 2 })).toEqual(0)
       })
     })
 
@@ -83,24 +79,16 @@ describe('getCacheValue', () => {
         const maxAge = 10
         await initCache({ current: 0, maxAge })
         await waitFor(5)
-        await execInParallelAndWaitForBackgroundTasks(async () => {
-          expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
-        })
-        await execInParallelAndWaitForBackgroundTasks(async () => {
-          expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(0)
-        })
+        expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
+        expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(0)
       })
 
       test('maxAge < passed time: returns cached value with background update', async () => {
         const maxAge = 10
         await initCache({ current: 0, maxAge })
         await waitFor(20)
-        await execInParallelAndWaitForBackgroundTasks(async () => {
-          expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
-        })
-        await execInParallelAndWaitForBackgroundTasks(async () => {
-          expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(1)
-        })
+        expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
+        expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(1)
       })
     })
   })
@@ -110,13 +98,13 @@ describe('getCacheValue', () => {
       const maxAge = 10
       await initCache({ current: 0, maxAge })
       await waitFor(20)
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
-        expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(0)
-      })
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 3, maxAge })).toEqual(1)
-      })
+      expect(
+        await Promise.all([
+          dataSource.getContent({ current: 1, maxAge }),
+          dataSource.getContent({ current: 2, maxAge }),
+        ])
+      ).toEqual([0, 0])
+      expect(await dataSource.getContent({ current: 3, maxAge })).toEqual(1)
     })
 
     test('only affects the same key', async () => {
@@ -124,78 +112,48 @@ describe('getCacheValue', () => {
       await initCache({ key: 'a', current: 0, maxAge })
       await initCache({ key: 'b', current: 5, maxAge })
       await waitFor(20)
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(
-          await dataSource.getContent({ key: 'a', current: 1, maxAge })
-        ).toEqual(0)
-        expect(
-          await dataSource.getContent({ key: 'b', current: 6, maxAge })
-        ).toEqual(5)
-      })
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(
-          await dataSource.getContent({ key: 'a', current: 2, maxAge })
-        ).toEqual(1)
-        expect(
-          await dataSource.getContent({ key: 'b', current: 7, maxAge })
-        ).toEqual(6)
-      })
+      expect(
+        await Promise.all([
+          dataSource.getContent({ key: 'a', current: 1, maxAge }),
+          dataSource.getContent({ key: 'b', current: 6, maxAge }),
+        ])
+      ).toEqual([0, 5])
+      expect(
+        await Promise.all([
+          dataSource.getContent({ key: 'a', current: 2, maxAge }),
+          dataSource.getContent({ key: 'b', current: 7, maxAge }),
+        ])
+      ).toEqual([1, 6])
     })
 
     test('is unlocked when update fails', async () => {
       const maxAge = 10
       await initCache({ key: 'key', current: 0, maxAge })
       await waitFor(20)
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        await dataSource.setCacheValue({
-          key: 'key',
-          update: () => {
-            throw new Error('Update fails')
-          },
-        })
+      await dataSource.setCacheValue({
+        key: 'key',
+        update: () => {
+          throw new Error('Update fails')
+        },
       })
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
-      })
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(1)
-      })
-    })
-
-    test('is unlocked after 10 seconds', async () => {
-      const maxAge = 20
-      await initCache({ current: 0, maxAge })
-      await waitFor(40)
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
-        await waitFor(10)
-        expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(0)
-      })
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 3, maxAge })).toEqual(2)
-      })
+      expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
+      expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(1)
     })
   })
 
   describe('legacy cache values', () => {
     test('passed time < maxAge: returns legacy value without background update', async () => {
       await cache.set('key', 0)
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 1 })).toEqual(0)
-      })
+      expect(await dataSource.getContent({ current: 1 })).toEqual(0)
       expect(await dataSource.getContent({ current: 2 })).toEqual(0)
     })
 
     test('maxAge < passed time: returns legacy value with background update', async () => {
       const maxAge = 10
       await cache.set('key', 0)
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
-      })
+      expect(await dataSource.getContent({ current: 1, maxAge })).toEqual(0)
       await waitFor(20)
-      await execInParallelAndWaitForBackgroundTasks(async () => {
-        expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(0)
-      })
+      expect(await dataSource.getContent({ current: 2, maxAge })).toEqual(0)
       expect(await dataSource.getContent({ current: 3 })).toEqual(2)
     })
   })
@@ -227,15 +185,4 @@ async function initCache(payload: CachePayload) {
 // eslint-disable-next-line @typescript-eslint/require-await
 async function waitFor(seconds: number) {
   now.mockReturnValue(now() + seconds * 1000)
-}
-
-async function execInParallelAndWaitForBackgroundTasks(
-  exec: () => Promise<void>
-) {
-  await exec()
-  await new Promise((resolve) => {
-    process.nextTick(() => {
-      resolve()
-    })
-  })
 }
