a8ecd05a49e3cd3bd939cfb0e614168e187c3da6
Stephan Kulla <git.mail@kulla.me>
2021-04-03 17:39:54 +0200

    Remove "fromEnum" help in decoder.ts

--- src/model/decoder.ts
+++ src/model/decoder.ts
@@ -485,11 +485,6 @@ enum NotificationEventType {
   SetUuidState = 'SetUuidStateNotificationEvent',
 }
 
-export const NotificationEventTypeDecoder = fromEnum(
-  'NotificationEventType',
-  NotificationEventType
-)
-
 export const AbstractNotificationEventDecoder = t.type({
   id: Uuid,
   instance: InstanceDecoder,
@@ -519,25 +514,3 @@ export const RemoveTaxonomyLinkNotificationEventDecoder = t.exact(
     }),
   ])
 )
-
-/**
- * Turns an enum into a io-ts decoder.
- *
- * Thanks to @haysmike's comment at
- * https://github.com/gcanti/io-ts/issues/216#issuecomment-621588750
- */
-function fromEnum<EnumType extends string>(
-  enumName: string,
-  theEnum: Record<string, EnumType>
-): t.Type<EnumType, EnumType, unknown> {
-  const isEnumValue = (input: unknown): input is EnumType =>
-    Object.values<unknown>(theEnum).includes(input)
-
-  return new t.Type<EnumType>(
-    enumName,
-    isEnumValue,
-    (input, context) =>
-      isEnumValue(input) ? t.success(input) : t.failure(input, context),
-    t.identity
-  )
-}
