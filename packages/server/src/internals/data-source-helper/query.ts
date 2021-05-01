c6a08c1bddaf49a37329a0a0881c1c98e80978cb
Stephan Kulla <git.mail@kulla.me>
2021-05-01 18:52:11 +0200

    Refactor data-source-helper/query.ts

--- packages/server/src/internals/data-source-helper/query.ts
+++ packages/server/src/internals/data-source-helper/query.ts
@@ -19,7 +19,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
  */
-import { option as O, either as E } from 'fp-ts'
+import { option as O } from 'fp-ts'
 import * as t from 'io-ts'
 import * as R from 'ramda'
 
@@ -28,45 +28,10 @@ import { Environment } from '../environment'
 import { Time } from '../swr-queue'
 import { InvalidValueError } from './common'
 
-export interface QuerySpec<P, R> {
-  // TODO: this should probably be required
-  decoder?: t.Type<R>
-  getCurrentValue: (payload: P, previousValue: R | null) => Promise<R | unknown>
-  enableSwr: boolean
-  swrFrequency?: number
-  maxAge: Time | undefined
-  getKey: (payload: P) => string
-  getPayload: (key: string) => O.Option<P>
-}
-
-export interface QuerySpecWithHelpers<P, R> extends QuerySpec<P, R> {
-  setCache: (
-    args: PayloadArrayOrPayload<P> & FunctionOrValue<R>
-  ) => Promise<void>
-  queryWithDecoder<S extends R>(
-    payload: P,
-    customDecoder: t.Type<S>
-  ): Promise<S>
-  queryWithDecoders<S2 extends R, S1 extends S2>(
-    payload: P,
-    customDecoder1: t.Type<S1>,
-    customDecoder2: t.Type<S2>
-  ): Promise<S2>
-}
-
-export type PayloadArrayOrPayload<P> = { payload: P } | { payloads: P[] }
-
-export type ModelQuery<P, R> = (P extends undefined
-  ? () => Promise<R>
-  : (payload: P) => Promise<R>) & {
-  _querySpec: QuerySpecWithHelpers<P, R>
-  __typename: 'CachedQuery'
-}
-
 export function createQuery<P, R>(
   spec: QuerySpec<P, R>,
   environment: Environment
-): ModelQuery<P, R> {
+): Query<P, R> {
   async function queryWithDecoder<S extends R>(
     payload: P,
     customDecoder?: t.Type<S>
@@ -77,33 +42,25 @@ export function createQuery<P, R>(
     const decoder = customDecoder ?? t.unknown
 
     if (O.isSome(cacheValue)) {
-      const cacheEntry = cacheValue.value
+      const { value } = cacheValue.value
 
-      const decoded = decoder.decode(cacheEntry.value)
-      if (E.isRight(decoded)) {
+      if (decoder.is(value)) {
         if (
           spec.swrFrequency === undefined ||
           Math.random() < spec.swrFrequency
         ) {
-          await environment.swrQueue.queue({
-            key,
-          })
+          await environment.swrQueue.queue({ key })
         }
 
-        return decoded.right as S
+        return value as S
       }
     }
 
     // Cache empty or invalid value
     const value = await spec.getCurrentValue(payload, null)
 
-    const decoded = decoder.decode(value)
-    if (E.isRight(decoded)) {
-      const value = decoded.right
-      await environment.cache.set({
-        key,
-        value,
-      })
+    if (decoder.is(value)) {
+      await environment.cache.set({ key, value })
       return value as S
     } else {
       throw new InvalidValueError(value)
@@ -132,25 +89,61 @@ export function createQuery<P, R>(
     queryWithDecoders,
     async setCache(args) {
       await Promise.all(
-        R.map(async (payload) => {
-          await environment.cache.set({
-            key: spec.getKey(payload),
-            ...args,
-          })
-        }, toPayloadArray(args))
+        toPayloadArray(args).map((payload) =>
+          environment.cache.set({ key: spec.getKey(payload), ...args })
+        )
       )
     },
   }
 
   query._querySpec = querySpecWithHelpers
-  query.__typename = 'CachedQuery'
+  query.__typename = 'Query'
 
-  return (query as unknown) as ModelQuery<P, R>
+  return (query as unknown) as Query<P, R>
 }
 
-export function isQuery(query: unknown): query is ModelQuery<unknown, unknown> {
-  return R.has('__typename', query) && query.__typename === 'CachedQuery'
+export function isQuery(query: unknown): query is Query<unknown, unknown> {
+  return R.has('__typename', query) && query.__typename === 'Query'
 }
+
+export interface QuerySpec<Payload, Result> {
+  // TODO: this should probably be required
+  decoder?: t.Type<Result>
+  getCurrentValue: (
+    payload: Payload,
+    previousValue: Result | null
+  ) => Promise<unknown>
+  enableSwr: boolean
+  swrFrequency?: number
+  maxAge: Time | undefined
+  getKey: (payload: Payload) => string
+  getPayload: (key: string) => O.Option<Payload>
+}
+
+export type Query<Payload, Result> = (Payload extends undefined
+  ? () => Promise<Result>
+  : (payload: Payload) => Promise<Result>) & {
+  _querySpec: QuerySpecWithHelpers<Payload, Result>
+  __typename: 'CachedQuery'
+}
+
+export interface QuerySpecWithHelpers<Payload, Result>
+  extends QuerySpec<Payload, Result> {
+  setCache: (
+    args: PayloadArrayOrPayload<Payload> & FunctionOrValue<Result>
+  ) => Promise<void>
+  queryWithDecoder<S extends Result>(
+    payload: Payload,
+    customDecoder: t.Type<S>
+  ): Promise<S>
+  queryWithDecoders<S2 extends Result, S1 extends S2>(
+    payload: Payload,
+    customDecoder1: t.Type<S1>,
+    customDecoder2: t.Type<S2>
+  ): Promise<S2>
+}
+
+type PayloadArrayOrPayload<P> = { payload: P } | { payloads: P[] }
 
 function toPayloadArray<P>(arg: PayloadArrayOrPayload<P>): P[] {
   return R.has('payloads', arg) ? arg.payloads : [arg.payload]
