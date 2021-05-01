6dc818956e4564b24fe376f840a921eebe14ea85
Stephan Kulla <git.mail@kulla.me>
2021-05-01 17:03:50 +0200

    Only re-export createMutation of ~/internals/model

--- packages/server/src/internals/model/index.ts
+++ packages/server/src/internals/model/index.ts
@@ -19,5 +19,5 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
  */
-export * from './mutation'
+export { createMutation } from './mutation'
 export * from './query'
