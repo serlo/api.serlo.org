0dd8f7073cadd12b2c575639f5b8eb13a47f4ada
Stephan Kulla <git.mail@kulla.me>
2021-05-02 12:48:30 +0200

    Add documentation to data-source-helper/query.ts

--- packages/server/src/internals/data-source-helper/query.ts
+++ packages/server/src/internals/data-source-helper/query.ts
@@ -28,6 +28,10 @@ import { Environment } from '../environment'
 import { Time } from '../swr-queue'
 import { InvalidValueError } from './common'
 
+/**
+ * Helper function to create a query in a data source. A query operation is a
+ * "read" operation whose result shall be cached by the API.
+ */
 export function createQuery<P, R>(
   spec: QuerySpec<P, R>,
   environment: Environment
@@ -102,40 +106,101 @@ export function createQuery<P, R>(
   return (query as unknown) as Query<P, R>
 }
 
+/**
+ * Type guard that a certain object is a query function created by
+ * {@link createQuery}.
+ */
 export function isQuery(query: unknown): query is Query<unknown, unknown> {
   return R.has('__typename', query) && query.__typename === 'Query'
 }
 
+/**
+ * Specification object to create a query function.
+ */
 export interface QuerySpec<Payload, Result> {
+  /**
+   * io-ts decoder to check whether the result of the operation or the cached
+   * value has the right type.
+   */
   // TODO: this should probably be required
   decoder?: t.Type<Result>
+
+  /**
+   * Function which gets the current value. The second parameter `previousValue`
+   * is the cached value and can be used to update the cached value to the
+   * current one.
+   */
   getCurrentValue: (
     payload: Payload,
     previousValue: Result | null
   ) => Promise<unknown>
+
+  /**
+   * Flag whether the SWR algorithm shall be used to update the cache in the
+   * background.
+   */
   enableSwr: boolean
+
+  /**
+   * Probability that a stale value shall be updated in the background when
+   * SWR is activated. This value shall be between 0 and 1.
+   */
   swrFrequency?: number
+
+  /**
+   * Age after which a value is considered to be stale in the SWR algorithm.
+   */
   maxAge: Time | undefined
+
+  /**
+   * Function which calculates the cache key based on the passed payload. It is
+   * important that cache keys are unique across all queries in all data
+   * sources.
+   */
   getKey: (payload: Payload) => string
+
+  /**
+   * The inverse function of {@link geKey}. It shall return `O.none` in case
+   * the cache key does not belong to this query.
+   */
   getPayload: (key: string) => O.Option<Payload>
 }
 
+/**
+ * Type of a query operation in a data source. Note that the specification
+ * object is extened by some helper functions.
+ */
 export type Query<Payload, Result> = (Payload extends undefined
   ? () => Promise<Result>
   : (payload: Payload) => Promise<Result>) & {
   _querySpec: QuerySpecWithHelpers<Payload, Result>
-  __typename: 'CachedQuery'
+  __typename: 'Query'
 }
 
+/**
+ * The specification object of a query extended by some helper functions.
+ */
 export interface QuerySpecWithHelpers<Payload, Result>
   extends QuerySpec<Payload, Result> {
-  setCache: (
+  /**
+   * Function to update the cache of one or many values.
+   */
+  setCache(
     args: PayloadArrayOrPayload<Payload> & FunctionOrValue<Result>
-  ) => Promise<void>
+  ): Promise<void>
+
+  /**
+   * Query function with a custom io-ts decoder.
+   */
   queryWithDecoder<S extends Result>(
     payload: Payload,
     customDecoder: t.Type<S>
   ): Promise<S>
+
+  /**
+   * Query function where two different io-ts decoder can be used. The second
+   * decoder is a fallback in case the first decoder does not match the result.
+   */
   queryWithDecoders<S2 extends Result, S1 extends S2>(
     payload: Payload,
     customDecoder1: t.Type<S1>,
