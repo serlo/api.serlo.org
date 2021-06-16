653c7868ecd056b3baa1d8c3880a23946b094d5c
Stephan Kulla <git.mail@kulla.me>
2021-06-16 13:40:42 +0200

    Move tests for set-state to own test file

---
+++ __tests__/schema/uuid/set-state.ts
@@ -0,0 +1,116 @@
+/**
+ * This file is part of Serlo.org API
+ *
+ * Copyright (c) 2020-2021 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2020-2021 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
+ */
+import { gql } from 'apollo-server'
+
+import { article, page, user } from '../../../__fixtures__'
+import {
+  assertFailingGraphQLMutation,
+  assertSuccessfulGraphQLMutation,
+  createTestClient,
+  givenUuidQueryEndpoint,
+  Client,
+  Database,
+  returnsUuidsFromDatabase,
+  givenSerloEndpoint,
+  MessageResolver,
+} from '../../__utils__'
+
+let database: Database
+let client: Client
+const mutation = gql`
+  mutation uuid($input: UuidSetStateInput!) {
+    uuid {
+      setState(input: $input) {
+        success
+      }
+    }
+  }
+`
+const articleIds = [article.id, article.id + 1]
+
+beforeEach(() => {
+  client = createTestClient({ userId: user.id })
+
+  database = new Database()
+  database.hasUuids([{ ...user, roles: ['de_architect'] }])
+
+  for (const articleId of articleIds) {
+    database.hasUuid({ ...article, id: articleId })
+  }
+
+  givenUuidQueryEndpoint(returnsUuidsFromDatabase(database))
+  givenUuidSetStateMutationEndpoint((req, res, ctx) => {
+    const { ids, trashed, userId } = req.body.payload
+
+    // In order to test whether these parameters are passed properly
+    if (userId !== user.id || trashed != true) {
+      return res(ctx.status(500))
+    }
+
+    for (const id of ids) {
+      database.changeUuid(id, { trashed })
+    }
+
+    return res(ctx.status(200))
+  })
+})
+
+test('authenticated with array of ids', async () => {
+  await assertSuccessfulGraphQLMutation({
+    mutation,
+    variables: {
+      input: { id: articleIds, trashed: true },
+    },
+    data: { uuid: { setState: { success: true } } },
+    client,
+  })
+})
+
+test('unauthenticated', async () => {
+  await assertFailingGraphQLMutation({
+    mutation,
+    variables: { input: { id: 1, trashed: true } },
+    client: createTestClient({ userId: null }),
+    expectedError: 'UNAUTHENTICATED',
+  })
+})
+
+test('insufficient permissions', async () => {
+  // Architects are not allowed to set the state of pages.
+  database.hasUuid(page)
+  await assertFailingGraphQLMutation({
+    mutation,
+    variables: { input: { id: page.id, trashed: false } },
+    client,
+    expectedError: 'FORBIDDEN',
+  })
+})
+
+function givenUuidSetStateMutationEndpoint(
+  resolver: MessageResolver<{
+    ids: number[]
+    userId: number
+    trashed: boolean
+  }>
+) {
+  givenSerloEndpoint('UuidSetStateMutation', resolver)
+}
