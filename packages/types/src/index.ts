40e262dced6792d6bdab30ae9d1a1bbeb05c8c75
Stephan Kulla <git.mail@kulla.me>
2021-10-22 16:48:23 +0200

    Add user.potentialSpamUsers

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1512,6 +1512,7 @@ export type Query = {
   notifications: NotificationConnection;
   subject: SubjectQuery;
   subscription: SubscriptionQuery;
+  user: UserQuery;
   uuid?: Maybe<AbstractUuid>;
 };
 
@@ -2238,6 +2239,17 @@ export type UserMutationDeleteRegularUsersArgs = {
 
 export type UserMutationSetEmailArgs = {
   input: UserSetEmailInput;
+};
+
+export type UserQuery = {
+  __typename?: 'UserQuery';
+  potentialSpamUsers: UserConnection;
+};
+
+
+export type UserQueryPotentialSpamUsersArgs = {
+  first?: Maybe<Scalars['Int']>;
+  after?: Maybe<Scalars['String']>;
 };
 
 export type UserSetEmailInput = {
