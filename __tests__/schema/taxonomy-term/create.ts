5c3ca34beaaec9981d0786962b4018d4ec34128e
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-04-14 11:41:20 +0200

    test(taxonomy-create): add basic tests

---
+++ __tests__/schema/taxonomy-term/create.ts
@@ -0,0 +1,104 @@
+/**
+ * This file is part of Serlo.org API
+ *
+ * Copyright (c) 2020-2022 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2020-2022 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
+ */
+import { gql } from 'apollo-server'
+
+import { taxonomyTermSubject, taxonomyTermCurriculumTopic, user } from '../../../__fixtures__'
+import { Client, given, nextUuid } from '../../__utils__'
+import { Instance, TaxonomyTermType } from '~/types'
+
+describe('TaxonomyTermCreateMutation', () => {
+  const input = {
+    parentId: taxonomyTermSubject.id,
+    instance: Instance.De,
+    name: 'a name ',
+    description: 'a description',
+  }
+
+  const mutation = new Client({ userId: user.id })
+    .prepareQuery({
+      query: gql`
+        mutation set($input: TaxonomyTermCreateInput!) {
+          taxonomyTerm {
+            create(input: $input) {
+              success
+              record {
+                id
+              }
+            }
+          }
+        }
+      `,
+    })
+    .withVariables({ input })
+
+  beforeEach(() => {
+    given('UuidQuery').for(user, taxonomyTermSubject)
+
+    given('TaxonomyTermCreateMutation')
+      .withPayload({
+        ...input,
+        userId: user.id,
+        taxonomyType: TaxonomyTermType.CurriculumTopic,
+      })
+      .returns(taxonomyTermCurriculumTopic)
+  })
+
+  test('returns { success, record } when mutation could be successfully executed', async () => {
+    await mutation.shouldReturnData({
+      taxonomyTerm: {
+        create: {
+          success: true,
+          record: { id: taxonomyTermCurriculumTopic.id },
+        },
+      },
+    })
+  })
+
+  test('fails when user is not authenticated', async () => {
+    await mutation
+      .forUnauthenticatedUser()
+      .shouldFailWithError('UNAUTHENTICATED')
+  })
+
+  test('fails when user does not have role "login"', async () => {
+    const guestUser = { ...user, id: nextUuid(user.id), roles: ['guest'] }
+
+    given('UuidQuery').for(guestUser)
+
+    await mutation
+      .forClient(new Client({ userId: guestUser.id }))
+      .withVariables({ input })
+      .shouldFailWithError('FORBIDDEN')
+  })
+
+  test('fails when database layer returns a 400er response', async () => {
+    given('TaxonomyTermCreateMutation').returnsBadRequest()
+
+    await mutation.shouldFailWithError('BAD_USER_INPUT')
+  })
+
+  test('fails when database layer has an internal error', async () => {
+    given('TaxonomyTermCreateMutation').hasInternalServerError()
+
+    await mutation.shouldFailWithError('INTERNAL_SERVER_ERROR')
+  })
+})
