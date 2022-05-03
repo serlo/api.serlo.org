1035dc3dc0a8d1cd5e1208181db2a7ef5da59119
Stephan Kulla <git.mail@kulla.me>
2022-05-03 11:27:00 +0200

    Fix lint errors for Query<...>

--- __tests__/__utils__/assertions.ts
+++ __tests__/__utils__/assertions.ts
@@ -37,19 +37,24 @@ export class Client {
     this.apolloServer = createTestClient(context)
   }
 
-  prepareQuery<V extends Variables = Variables>(query: QuerySpec<V>) {
+  prepareQuery<I extends Input = Input, V extends Variables<I> = Variables<I>>(
+    query: QuerySpec<V>
+  ) {
     return new Query(this, query)
   }
 
-  execute(query: QuerySpec<Variables>) {
+  execute(query: QuerySpec<Variables<Input>>) {
     return this.apolloServer.executeOperation(query)
   }
 }
 
-export class Query<V extends Variables = Variables> {
+export class Query<
+  I extends Input = Input,
+  V extends Variables<I> = Variables<I>
+> {
   constructor(private client: Client, private query: QuerySpec<V>) {}
 
-  withInput(input: 'input' extends keyof V ? V['input'] : never) {
+  withInput(input: I) {
     return new Query(this.client, { ...this.query, variables: { input } })
   }
 
@@ -97,7 +102,8 @@ export class Query<V extends Variables = Variables> {
   }
 }
 
-type Variables = Record<string, unknown> & { input?: Record<string, unknown> }
+type Variables<I> = { input: I } | Record<string, unknown>
+type Input = Record<string, unknown>
 interface QuerySpec<V> {
   query: DocumentNode
   variables?: V
