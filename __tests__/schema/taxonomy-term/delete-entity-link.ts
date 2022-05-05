a785f5c5c067cfe5a826147c6ef35601f99a94b4
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-05-05 14:03:07 +0200

    test(taxonomy-delete-entity-link): remove unnecessary lines,minor change

--- __tests__/schema/taxonomy-term/delete-entity-link.ts
+++ __tests__/schema/taxonomy-term/delete-entity-link.ts
@@ -28,158 +28,151 @@ import {
 } from '../../../__fixtures__'
 import { Client, given } from '../../__utils__'
 
-describe('TaxonomyDeleteEntityLinkMutation', () => {
-  const user = { ...baseUser, roles: ['de_architect'] }
+const user = { ...baseUser, roles: ['de_architect'] }
 
-  const input = {
-    entityIds: [article.id],
-    taxonomyTermId: taxonomyTermCurriculumTopic.id,
-  }
+const input = {
+  entityIds: [article.id],
+  taxonomyTermId: taxonomyTermCurriculumTopic.id,
+}
 
-  const mutation = new Client({ userId: user.id })
-    .prepareQuery({
-      query: gql`
-        mutation set($input: TaxonomyEntityLinkInput!) {
-          taxonomyTerm {
-            deleteEntityLink(input: $input) {
-              success
-            }
+const mutation = new Client({ userId: user.id })
+  .prepareQuery({
+    query: gql`
+      mutation set($input: TaxonomyEntityLinkInput!) {
+        taxonomyTerm {
+          deleteEntityLink(input: $input) {
+            success
           }
         }
-      `,
-    })
-    .withVariables({ input })
+      }
+    `,
+  })
+  .withInput(input)
 
-  beforeEach(() => {
-    given('UuidQuery').for(
-      { ...article, taxonomyTermIds: [taxonomyTermCurriculumTopic.id] },
-      taxonomyTermCurriculumTopic,
-      user
-    )
+beforeEach(() => {
+  given('UuidQuery').for(
+    { ...article, taxonomyTermIds: [taxonomyTermCurriculumTopic.id] },
+    taxonomyTermCurriculumTopic,
+    user
+  )
 
-    given('TaxonomyDeleteEntityLinkMutation')
-      .withPayload({ ...input, userId: user.id })
-      .isDefinedBy((_req, res, ctx) => {
-        given('UuidQuery').for({
-          ...article,
-          taxonomyTermIds: [],
-        })
-        given('UuidQuery').for({
-          ...taxonomyTermCurriculumTopic,
-          childrenIds: [],
-        })
-        return res(ctx.json({ success: true }))
+  given('TaxonomyDeleteEntityLinkMutation')
+    .withPayload({ ...input, userId: user.id })
+    .isDefinedBy((_req, res, ctx) => {
+      given('UuidQuery').for({
+        ...article,
+        taxonomyTermIds: [],
       })
-  })
-
-  test('returns { success, record } when mutation could be successfully executed', async () => {
-    await mutation.shouldReturnData({
-      taxonomyTerm: {
-        deleteEntityLink: {
-          success: true,
-        },
-      },
+      given('UuidQuery').for({
+        ...taxonomyTermCurriculumTopic,
+        childrenIds: [],
+      })
+      return res(ctx.json({ success: true }))
     })
-  })
+})
 
-  test('updates the cache', async () => {
-    const childQuery = new Client({ userId: user.id }).prepareQuery({
-      query: gql`
-        query ($id: Int!) {
-          uuid(id: $id) {
-            ... on Article {
-              taxonomyTerms {
-                nodes {
-                  id
-                }
+test('returns { success, record } when mutation could be successfully executed', async () => {
+  await mutation.shouldReturnData({
+    taxonomyTerm: {
+      deleteEntityLink: {
+        success: true,
+      },
+    },
+  })
+})
+
+test('updates the cache', async () => {
+  const childQuery = new Client({ userId: user.id }).prepareQuery({
+    query: gql`
+      query ($id: Int!) {
+        uuid(id: $id) {
+          ... on Article {
+            taxonomyTerms {
+              nodes {
+                id
               }
             }
           }
         }
-      `,
-      variables: { id: article.id },
-    })
-    await childQuery.shouldReturnData({
-      uuid: {
-        taxonomyTerms: {
-          nodes: [
-            {
-              id: taxonomyTermCurriculumTopic.id,
-            },
-          ],
-        },
+      }
+    `,
+    variables: { id: article.id },
+  })
+  await childQuery.shouldReturnData({
+    uuid: {
+      taxonomyTerms: {
+        nodes: [
+          {
+            id: taxonomyTermCurriculumTopic.id,
+          },
+        ],
       },
-    })
+    },
+  })
 
-    const parentQuery = new Client({ userId: user.id }).prepareQuery({
-      query: gql`
-        query ($id: Int!) {
-          uuid(id: $id) {
-            ... on TaxonomyTerm {
-              name
-              children {
-                nodes {
-                  id
-                }
+  const parentQuery = new Client({ userId: user.id }).prepareQuery({
+    query: gql`
+      query ($id: Int!) {
+        uuid(id: $id) {
+          ... on TaxonomyTerm {
+            children {
+              nodes {
+                id
               }
             }
           }
         }
-      `,
-      variables: { id: taxonomyTermCurriculumTopic.id },
-    })
+      }
+    `,
+    variables: { id: taxonomyTermCurriculumTopic.id },
+  })
 
-    await parentQuery.shouldReturnData({
-      uuid: {
-        name: taxonomyTermCurriculumTopic.name,
-        children: {
-          nodes: [
-            {
-              id: article.id,
-            },
-          ],
-        },
+  await parentQuery.shouldReturnData({
+    uuid: {
+      children: {
+        nodes: [
+          {
+            id: article.id,
+          },
+        ],
       },
-    })
+    },
+  })
 
-    await mutation.execute()
+  await mutation.execute()
 
-    await parentQuery.shouldReturnData({
-      uuid: {
-        name: taxonomyTermCurriculumTopic.name,
-        children: {
-          nodes: [],
-        },
+  await parentQuery.shouldReturnData({
+    uuid: {
+      children: {
+        nodes: [],
       },
-    })
-    await childQuery.shouldReturnData({
-      uuid: {
-        taxonomyTerms: {
-          nodes: [],
-        },
+    },
+  })
+  await childQuery.shouldReturnData({
+    uuid: {
+      taxonomyTerms: {
+        nodes: [],
       },
-    })
+    },
   })
+})
 
-  test('fails when user is not authenticated', async () => {
-    await mutation
-      .forUnauthenticatedUser()
-      .shouldFailWithError('UNAUTHENTICATED')
-  })
+test('fails when user is not authenticated', async () => {
+  await mutation.forUnauthenticatedUser().shouldFailWithError('UNAUTHENTICATED')
+})
 
-  test('fails when user does not have role "architect"', async () => {
-    await mutation.forLoginUser().shouldFailWithError('FORBIDDEN')
-  })
+test('fails when user does not have role "architect"', async () => {
+  await mutation.forLoginUser().shouldFailWithError('FORBIDDEN')
+})
 
-  test('fails when database layer returns a 400er response', async () => {
-    given('TaxonomyDeleteEntityLinkMutation').returnsBadRequest()
+test('fails when database layer returns a 400er response', async () => {
+  given('TaxonomyDeleteEntityLinkMutation').returnsBadRequest()
 
-    await mutation.shouldFailWithError('BAD_USER_INPUT')
-  })
+  await mutation.shouldFailWithError('BAD_USER_INPUT')
+})
 
-  test('fails when database layer has an internal error', async () => {
-    given('TaxonomyDeleteEntityLinkMutation').hasInternalServerError()
+test('fails when database layer has an internal error', async () => {
+  given('TaxonomyDeleteEntityLinkMutation').hasInternalServerError()
 
-    await mutation.shouldFailWithError('INTERNAL_SERVER_ERROR')
-  })
+  await mutation.shouldFailWithError('INTERNAL_SERVER_ERROR')
 })
