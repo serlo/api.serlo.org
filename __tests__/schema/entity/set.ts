20e310bc9abc9a3144f5c9e52b562196aca3c5db
Stephan Kulla <git.mail@kulla.me>
2022-05-06 16:46:49 +0200

    feat(entity-create): Set default license by instance

--- __tests__/schema/entity/set.ts
+++ __tests__/schema/entity/set.ts
@@ -54,6 +54,7 @@ import { DatabaseLayer } from '~/model'
 import { castToUuid, EntityType } from '~/model/decoder'
 import { SetAbstractEntityInput } from '~/schema/uuid/abstract-entity/entity-set-handler'
 import { fromEntityTypeToEntityRevisionType } from '~/schema/uuid/abstract-entity/utils'
+import { Instance } from '~/types'
 
 interface EntityFields {
   title: string
@@ -519,6 +520,51 @@ testCases.forEach((testCase) => {
       })
     })
   })
+})
+
+test('uses default license of the instance', async () => {
+  const exerciseEn = { ...exercise, instance: Instance.En }
+
+  given('UuidQuery').for(exerciseEn, taxonomyTermSubject, taxonomyTermRoot)
+  given('EntityCreateMutation')
+    .withPayload({
+      userId: 1,
+      entityType: EntityType.Solution,
+      input: {
+        changes: 'changes',
+        licenseId: 9,
+        needsReview: true,
+        subscribeThis: true,
+        subscribeThisByEmail: true,
+        fields: { content: 'Hello World' },
+        parentId: exerciseEn.id,
+      },
+    })
+    .returns(solution)
+
+  await new Client({ userId: user.id })
+    .prepareQuery({
+      query: gql`
+        mutation ($input: SetGenericEntityInput!) {
+          entity {
+            setSolution(input: $input) {
+              success
+            }
+          }
+        }
+      `,
+      variables: {
+        input: {
+          changes: 'changes',
+          subscribeThis: true,
+          subscribeThisByEmail: true,
+          needsReview: true,
+          parentId: exerciseEn.id,
+          content: 'Hello World',
+        },
+      },
+    })
+    .shouldReturnData({ entity: { setSolution: { success: true } } })
 })
 
 describe('Autoreview entities', () => {
