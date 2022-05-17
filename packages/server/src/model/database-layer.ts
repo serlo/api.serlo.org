553f051e1a3232445353f0011e7db98705cb1e60
Stephan Kulla <git.mail@kulla.me>
2022-05-17 15:48:59 +0200

    refactor: Delete TaxonomyTypeCreateOptionsDecoder

--- packages/server/src/model/database-layer.ts
+++ packages/server/src/model/database-layer.ts
@@ -36,7 +36,6 @@ import {
   PageDecoder,
   SubscriptionsDecoder,
   TaxonomyTermDecoder,
-  TaxonomyTypeCreateOptionsDecoder,
   Uuid,
   UuidDecoder,
 } from './decoder'
@@ -311,7 +310,7 @@ export const spec = {
   },
   TaxonomyTermCreateMutation: {
     payload: t.type({
-      taxonomyType: TaxonomyTypeCreateOptionsDecoder,
+      taxonomyType: t.union([t.literal('topic'), t.literal('topic-folder')]),
       name: t.string,
       userId: t.number,
       description: t.union([t.string, t.null, t.undefined]),
