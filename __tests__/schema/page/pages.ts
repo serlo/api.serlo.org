52a334392ead5d989f7230c390fba167d81212fb
MoeHome <moritz@heimbaecher.eu>
2022-05-31 13:03:28 +0200

    fixed code and tests

--- __tests__/schema/page/pages.ts
+++ __tests__/schema/page/pages.ts
@@ -37,9 +37,9 @@ beforeEach(() => {
 
   query = client.prepareQuery({
     query: gql`
-      query ($input: PagesInput!) {
+      query ($instance: Instance) {
         page {
-          pages(input: $input) {
+          pages(instance: $instance) {
             success
             pages {
               id
@@ -48,48 +48,49 @@ beforeEach(() => {
         }
       }
     `,
-    variables: { input: {} },
   })
 
   given('UuidQuery').for(page, page2)
-  given('PageQuery').isDefinedBy((req, res, ctx) => {
+  given('PagesQuery').isDefinedBy((req, res, ctx) => {
     const { instance } = req.body.payload
 
     if (instance === Instance.En) {
-      return res(ctx.json({ success: true, pages: [page] }))
+      return res(ctx.json({ success: true, pages: [page.id] }))
     }
-    return res(ctx.json({ success: true, pages: [page, page2] }))
+    return res(ctx.json({ success: true, pages: [page.id, page2.id] }))
   })
 })
 
 test('returns all pages', async () => {
-  await query
-    .withVariables({ input: { instance: Instance.En } })
-    .shouldReturnData({
-      page: {
-        pages: {
-          success: true,
-          pages: [page, page2],
-        },
+  await query.shouldReturnData({
+    page: {
+      pages: {
+        success: true,
+        pages: [{ id: page.id }, { id: page2.id }],
       },
-    })
+    },
+  })
 })
 
 test('returns english pages', async () => {
-  await query
-    .withVariables({ input: { instance: Instance.En } })
-    .shouldReturnData({
-      page: {
-        pages: {
-          success: true,
-          pages: [page],
-        },
+  await query.withVariables({ instance: Instance.En }).shouldReturnData({
+    page: {
+      pages: {
+        success: true,
+        pages: [{ id: page.id }],
       },
-    })
+    },
+  })
 })
 
 test('fails when database layer has an internal error', async () => {
-  given('PageQuery').hasInternalServerError()
+  given('PagesQuery').hasInternalServerError()
 
   await query.shouldFailWithError('INTERNAL_SERVER_ERROR')
+})
+
+test('fails when database layer returns a 400er response', async () => {
+  given('PagesQuery').returnsBadRequest()
+
+  await query.shouldFailWithError('BAD_USER_INPUT')
 })
