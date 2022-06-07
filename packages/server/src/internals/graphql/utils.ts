68ff6c4bc361b53e498742be29a62f9de81b4801
Stephan Kulla <git.mail@kulla.me>
2022-06-07 23:50:42 +0200

    fix: Mandatory fields in Entity.setRevision() cannot only contain spaces
    Closes #656

--- packages/server/src/internals/graphql/utils.ts
+++ packages/server/src/internals/graphql/utils.ts
@@ -107,7 +107,9 @@ export function decodeFromBase64(text: string) {
 
 export function assertArgumentIsNotEmpty(args: { [key: string]: unknown }) {
   const emptyArgs: string[] = Object.entries(args)
-    .filter(([_, value]) => !value)
+    .filter(
+      ([_, value]) => typeof value === 'string' && value.trim().length === 0
+    )
     .map(([key]) => key)
 
   if (emptyArgs.length > 0) {
