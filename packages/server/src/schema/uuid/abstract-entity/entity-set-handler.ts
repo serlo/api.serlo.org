c4e00dcc6d412c1e1b880cfe70dbdfe468bb2d8c
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-06-09 13:41:00 +0200

    refactor: rename assertArgumentIsNotEmpty to assertStringIsNotEmpty

--- packages/server/src/schema/uuid/abstract-entity/entity-set-handler.ts
+++ packages/server/src/schema/uuid/abstract-entity/entity-set-handler.ts
@@ -27,7 +27,7 @@ import * as R from 'ramda'
 import { fromEntityTypeToEntityRevisionType } from './utils'
 import { autoreviewTaxonomyIds, getDefaultLicense } from '~/config'
 import {
-  assertArgumentIsNotEmpty,
+  assertStringIsNotEmpty,
   assertUserIsAuthenticated,
   assertUserIsAuthorized,
   Context,
@@ -66,7 +66,7 @@ export function createSetEntityResolver({
     { input }: { input: SetAbstractEntityInput },
     { dataSources, userId }: Context
   ) => {
-    assertArgumentIsNotEmpty(R.pick(mandatoryFieldKeys, input))
+    assertStringIsNotEmpty(R.pick(mandatoryFieldKeys, input))
 
     if (transformedInput != null) input = transformedInput(input)
 
