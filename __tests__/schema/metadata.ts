1b8145d5f4ec8b10d9706c3191e7002d3ebb1259
Botho <1258870+elbotho@users.noreply.github.com>
2022-06-13 10:25:58 +0200

    version: add test case for 0.0.0

--- __tests__/schema/metadata.ts
+++ __tests__/schema/metadata.ts
@@ -169,19 +169,24 @@ describe('endpoint "entities"', () => {
 })
 
 test('endpoint `version` returns string that could be semver', async () => {
-  await new Client()
-    .prepareQuery({
-      query: gql`
-        query {
-          metadata {
-            version
-          }
+  const versionQuery = new Client().prepareQuery({
+    query: gql`
+      query {
+        metadata {
+          version
         }
-      `,
-    })
-    .shouldReturnData({
-      metadata: {
-        version: expect.stringMatching(/^\d+\.\d+\.\d+/) as unknown,
-      },
-    })
+      }
+    `,
+  })
+  await versionQuery.shouldReturnData({
+    metadata: {
+      version: expect.not.stringContaining('0.0.0') as unknown,
+    },
+  })
+
+  await versionQuery.shouldReturnData({
+    metadata: {
+      version: expect.stringMatching(/^\d+\.\d+\.\d+/) as unknown,
+    },
+  })
 })
