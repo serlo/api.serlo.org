4e1d66d32769661eaa0ee2f010b4b9134bf501d8
Botho <1258870+elbotho@users.noreply.github.com>
2022-06-16 15:17:51 +0200

    TaxonomyTermCreateMutation: test exerciseFolder

--- __tests__/schema/taxonomy-term/create.ts
+++ __tests__/schema/taxonomy-term/create.ts
@@ -34,159 +34,216 @@ import { TaxonomyTypeCreateOptions } from '~/types'
 describe('TaxonomyTermCreateMutation', () => {
   const user = { ...baseUser, roles: ['de_architect'] }
 
-  // TODO: Write test for TaxonomyTypeCreateOptions.ExerciseFolder
+  describe('create Topic', () => {
+    const input = {
+      parentId: taxonomyTermSubject.id,
+      name: 'a name ',
+      description: 'a description',
+      taxonomyType: TaxonomyTypeCreateOptions.Topic,
+    }
 
-  const input = {
-    parentId: taxonomyTermSubject.id,
-    name: 'a name ',
-    description: 'a description',
-    taxonomyType: TaxonomyTypeCreateOptions.Topic,
-  }
-
-  const mutation = new Client({ userId: user.id })
-    .prepareQuery({
-      query: gql`
-        mutation set($input: TaxonomyTermCreateInput!) {
-          taxonomyTerm {
-            create(input: $input) {
-              success
-              record {
-                id
+    const mutation = new Client({ userId: user.id })
+      .prepareQuery({
+        query: gql`
+          mutation set($input: TaxonomyTermCreateInput!) {
+            taxonomyTerm {
+              create(input: $input) {
+                success
+                record {
+                  id
+                }
               }
             }
           }
-        }
-      `,
-    })
-    .withVariables({ input })
-
-  const payload = {
-    ...input,
-    taxonomyType: 'topic' as const,
-    userId: user.id,
-  }
-
-  beforeEach(() => {
-    given('UuidQuery').for(user, taxonomyTermSubject)
-
-    given('TaxonomyTermCreateMutation')
-      .withPayload(payload)
-      .returns(taxonomyTermTopic)
-  })
-
-  test('returns { success, record } when mutation could be successfully executed', async () => {
-    await mutation.shouldReturnData({
-      taxonomyTerm: {
-        create: {
-          success: true,
-          record: { id: taxonomyTermTopic.id },
-        },
-      },
-    })
-  })
-
-  test('updates the cache', async () => {
-    given('UuidQuery').for(taxonomyTermCurriculumTopic)
-
-    given('TaxonomyTermCreateMutation')
-      .withPayload(payload)
-      .isDefinedBy((_req, res, ctx) => {
-        given('UuidQuery').for(taxonomyTermTopic)
-
-        const updatedParent = {
-          ...taxonomyTermSubject,
-          childrenIds: [
-            ...taxonomyTermSubject.childrenIds,
-            taxonomyTermTopic.id,
-          ],
-        }
-        given('UuidQuery').for(updatedParent)
-        return res(ctx.json(taxonomyTermTopic))
+        `,
       })
+      .withVariables({ input })
 
-    const query = new Client({ userId: user.id }).prepareQuery({
-      query: gql`
-        query ($id: Int!) {
-          uuid(id: $id) {
-            ... on TaxonomyTerm {
-              name
-              children {
-                nodes {
-                  ... on TaxonomyTerm {
-                    id
-                    name
+    const payload = {
+      ...input,
+      taxonomyType: 'topic' as const,
+      userId: user.id,
+    }
+
+    beforeEach(() => {
+      given('UuidQuery').for(user, taxonomyTermSubject)
+
+      given('TaxonomyTermCreateMutation')
+        .withPayload(payload)
+        .returns(taxonomyTermTopic)
+    })
+
+    test('returns { success, record } when mutation could be successfully executed', async () => {
+      await mutation.shouldReturnData({
+        taxonomyTerm: {
+          create: {
+            success: true,
+            record: { id: taxonomyTermTopic.id },
+          },
+        },
+      })
+    })
+
+    test('updates the cache', async () => {
+      given('UuidQuery').for(taxonomyTermCurriculumTopic)
+
+      given('TaxonomyTermCreateMutation')
+        .withPayload(payload)
+        .isDefinedBy((_req, res, ctx) => {
+          given('UuidQuery').for(taxonomyTermTopic)
+
+          const updatedParent = {
+            ...taxonomyTermSubject,
+            childrenIds: [
+              ...taxonomyTermSubject.childrenIds,
+              taxonomyTermTopic.id,
+            ],
+          }
+          given('UuidQuery').for(updatedParent)
+          return res(ctx.json(taxonomyTermTopic))
+        })
+
+      const query = new Client({ userId: user.id }).prepareQuery({
+        query: gql`
+          query ($id: Int!) {
+            uuid(id: $id) {
+              ... on TaxonomyTerm {
+                name
+                children {
+                  nodes {
+                    ... on TaxonomyTerm {
+                      id
+                      name
+                    }
                   }
                 }
               }
             }
           }
-        }
-      `,
-      variables: { id: taxonomyTermSubject.id },
-    })
+        `,
+        variables: { id: taxonomyTermSubject.id },
+      })
 
-    await query.shouldReturnData({
-      uuid: {
-        name: taxonomyTermSubject.name,
-        children: {
-          nodes: [
-            {
-              id: taxonomyTermSubject.childrenIds[0],
-              name: taxonomyTermCurriculumTopic.name,
-            },
-          ],
+      await query.shouldReturnData({
+        uuid: {
+          name: taxonomyTermSubject.name,
+          children: {
+            nodes: [
+              {
+                id: taxonomyTermSubject.childrenIds[0],
+                name: taxonomyTermCurriculumTopic.name,
+              },
+            ],
+          },
         },
-      },
-    })
+      })
 
-    await mutation.execute()
+      await mutation.execute()
 
-    await query.shouldReturnData({
-      uuid: {
-        name: taxonomyTermSubject.name,
-        children: {
-          nodes: [
-            {
-              id: taxonomyTermSubject.childrenIds[0],
-              name: taxonomyTermCurriculumTopic.name,
-            },
-            {
-              id: taxonomyTermTopic.id,
-              name: taxonomyTermTopic.name,
-            },
-          ],
+      await query.shouldReturnData({
+        uuid: {
+          name: taxonomyTermSubject.name,
+          children: {
+            nodes: [
+              {
+                id: taxonomyTermSubject.childrenIds[0],
+                name: taxonomyTermCurriculumTopic.name,
+              },
+              {
+                id: taxonomyTermTopic.id,
+                name: taxonomyTermTopic.name,
+              },
+            ],
+          },
         },
-      },
+      })
+    })
+
+    test('fails when parent does not accept topic', async () => {
+      given('UuidQuery').for(taxonomyTermRoot)
+
+      await mutation
+        .withVariables({ ...input, parentId: taxonomyTermRoot.id })
+        .shouldFailWithError('BAD_USER_INPUT')
+    })
+
+    test('fails when user is not authenticated', async () => {
+      await mutation
+        .forUnauthenticatedUser()
+        .shouldFailWithError('UNAUTHENTICATED')
+    })
+
+    test('fails when user does not have role "architect"', async () => {
+      await mutation.forLoginUser().shouldFailWithError('FORBIDDEN')
+    })
+
+    test('fails when database layer returns a 400er response', async () => {
+      given('TaxonomyTermCreateMutation').returnsBadRequest()
+
+      await mutation.shouldFailWithError('BAD_USER_INPUT')
+    })
+
+    test('fails when database layer has an internal error', async () => {
+      given('TaxonomyTermCreateMutation').hasInternalServerError()
+
+      await mutation.shouldFailWithError('INTERNAL_SERVER_ERROR')
     })
   })
 
-  test('fails when parent does not accept topic or exerciseFolder', async () => {
-    given('UuidQuery').for(taxonomyTermRoot)
+  describe('create ExerciseFolder', () => {
+    const input = {
+      parentId: taxonomyTermSubject.id,
+      name: 'a name ',
+      description: 'a description',
+      taxonomyType: TaxonomyTypeCreateOptions.Topic,
+    }
 
-    await mutation
-      .withVariables({ ...input, parentId: taxonomyTermRoot.id })
-      .shouldFailWithError('BAD_USER_INPUT')
-  })
+    const mutation = new Client({ userId: user.id })
+      .prepareQuery({
+        query: gql`
+          mutation set($input: TaxonomyTermCreateInput!) {
+            taxonomyTerm {
+              create(input: $input) {
+                success
+                record {
+                  id
+                }
+              }
+            }
+          }
+        `,
+      })
+      .withVariables({ input })
 
-  test('fails when user is not authenticated', async () => {
-    await mutation
-      .forUnauthenticatedUser()
-      .shouldFailWithError('UNAUTHENTICATED')
-  })
+    const payload = {
+      ...input,
+      taxonomyType: 'topic' as const,
+      userId: user.id,
+    }
 
-  test('fails when user does not have role "architect"', async () => {
-    await mutation.forLoginUser().shouldFailWithError('FORBIDDEN')
-  })
+    beforeEach(() => {
+      given('UuidQuery').for(user, taxonomyTermSubject)
 
-  test('fails when database layer returns a 400er response', async () => {
-    given('TaxonomyTermCreateMutation').returnsBadRequest()
+      given('TaxonomyTermCreateMutation')
+        .withPayload(payload)
+        .returns(taxonomyTermTopic)
+    })
 
-    await mutation.shouldFailWithError('BAD_USER_INPUT')
-  })
+    test('returns { success, record } when mutation could be successfully executed', async () => {
+      await mutation.shouldReturnData({
+        taxonomyTerm: {
+          create: {
+            success: true,
+            record: { id: taxonomyTermTopic.id },
+          },
+        },
+      })
+    })
 
-  test('fails when database layer has an internal error', async () => {
-    given('TaxonomyTermCreateMutation').hasInternalServerError()
-
-    await mutation.shouldFailWithError('INTERNAL_SERVER_ERROR')
+    test('fails when parent does not accept exerciseFolder', async () => {
+      await mutation
+        .withVariables({ ...input, parentId: taxonomyTermSubject.id })
+        .shouldFailWithError('BAD_USER_INPUT')
+    })
   })
 })
