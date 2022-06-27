b6159139727d543d218fac4eb29a67fee5b17898
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-06-28 01:04:47 +0200

    feat(kratos): create new user in kratos and dblayer

--- packages/server/src/internals/server/kratos-middleware.ts
+++ packages/server/src/internals/server/kratos-middleware.ts
@@ -19,7 +19,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/api.serlo.org for the canonical source repository
  */
-import { Configuration, V0alpha2Api } from '@ory/kratos-client'
+import { Configuration, IdentityState, V0alpha2Api } from '@ory/kratos-client'
 import { Express, RequestHandler } from 'express'
 
 import { DatabaseLayer } from '~/model'
@@ -39,47 +39,63 @@ const kratos = new V0alpha2Api(
 
 function createKratosRegisterEndpoint(): RequestHandler {
   return async (req, res) => {
-    const { username, password, email } = req.body as {
-      username: string
-      password: string
-      email: string
-    }
+    try {
+      const { username, password, email } = req.body as {
+        username: string
+        password: string
+        email: string
+      }
 
-    const newUser = {
-      traits: {
-        username,
-        email,
-      },
-      credentials: {
-        password: {
-          config: {
-            password,
+      const newUser = {
+        schema_id: 'default',
+        traits: {
+          username,
+          email,
+        },
+        credentials: {
+          password: {
+            config: {
+              password,
+            },
           },
         },
-      },
-    }
-    const user = (await kratos.adminCreateIdentity(newUser)) as {
-      data: { id: string }
-    }
+      }
+      const user = (await kratos.adminCreateIdentity(newUser)) as {
+        data: { id: string }
+      }
 
-    const userId = (
-      await DatabaseLayer.makeRequest('UserCreateMutation', {
+      const payload = {
         username,
         password: user.data.id,
         email,
-      })
-    ).userId
+      }
+      const userId = (
+        (await DatabaseLayer.makeRequest('UserCreateMutation', payload)) as {
+          userId: number
+        }
+      ).userId
 
-    await kratos.adminUpdateIdentity(user.data.id, {
-      metadata_public: {
-        legacy_id: userId,
-      },
-    })
-    res.statusCode = 200
-    res.end(
-      JSON.stringify({
-        status: 'success',
+      await kratos.adminUpdateIdentity(user.data.id, {
+        schema_id: 'default',
+        metadata_public: {
+          legacy_id: userId,
+        },
+        traits: {
+          username,
+          email,
+        },
+        state: IdentityState.Active,
       })
-    )
+      res.statusCode = 200
+      res.end(
+        JSON.stringify({
+          status: 'success',
+        })
+      )
+    } catch (error: unknown) {
+      // TODO: delete user im kratos and db layer
+      res.statusCode = 400
+      res.end(JSON.stringify(error))
+    }
   }
 }
