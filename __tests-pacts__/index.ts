69866ffb22cb5e31ea823afee79ee1219559bc27
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-07-21 23:09:34 +0200

    test(pacts): match UserAddRoleMutaton payload to a existent in test db
    This time, for real.

--- __tests-pacts__/index.ts
+++ __tests-pacts__/index.ts
@@ -566,7 +566,7 @@ const pactSpec: PactSpec = {
       [
         {
           username: '1229f9c7',
-          roleName: 'reviewer',
+          roleName: 'german_reviewer',
         },
         {
           success: true,
