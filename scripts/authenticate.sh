9043e5fc452571fd2e5db61a53198f5cdb6f3fc4
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-08-18 23:06:29 +0200

    chore(auth-script): minor changes

--- scripts/authenticate.sh
+++ scripts/authenticate.sh
@@ -8,21 +8,23 @@ else
   user_id=$1
 fi
 
-sed -i -e '103,132d' packages/server/src/internals/server/graphql-middleware.ts
-sed -i "s/context({ req }): Promise<Pick<Context, 'service' | 'userId'>> {/context(): Promise<Pick<Context, 'service' | 'userId'>> {return Promise.resolve({ service: Service.SerloCloudflareWorker,userId: $user_id,}) }/" packages/server/src/internals/server/graphql-middleware.ts
+auth_file=packages/server/src/internals/server/graphql-middleware.ts
+
+sed -i -e '103,132d' $auth_file
+sed -i "s/context({ req }): Promise<Pick<Context, 'service' | 'userId'>> {/context(): Promise<Pick<Context, 'service' | 'userId'>> {return Promise.resolve({ service: Service.SerloCloudflareWorker,userId: $user_id,}) }/" $auth_file
 
 print_header "Authenticated as user with id $user_id"
-echo "The file 'packages/server/src/internals/server/graphql-middleware.ts' was changed in order to allow authentication."
+echo "The file '$auth_file' was changed in order to allow authentication."
 echo "Important: Do not commit this file!"
 echo "If you are editing this file, do not use this script!"
 echo "Exit: ctrl+C"
 
 trap restore_graphql_middleware SIGINT
 
-restore_graphql_middleware() {
+function restore_graphql_middleware() {
   echo
-  echo "Restoring 'packages/server/src/internals/server/graphql-middleware.ts' to original state"
-  git restore packages/server/src/internals/server/graphql-middleware.ts
+  echo "Restoring '$auth_file' to original state"
+  git restore $auth_file
 }
 
 sleep 99999m
