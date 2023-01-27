f5556cd8ce273431daf7f85cda67d46651992762
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-01-27 23:06:45 +0100

    Format

--- packages/server/src/model/serlo.ts
+++ packages/server/src/model/serlo.ts
@@ -57,10 +57,7 @@ export function createSerloModel({
       decoder: DatabaseLayer.getDecoderFor('UuidQuery'),
       enableSwr: true,
       getCurrentValue: async (payload: DatabaseLayer.Payload<'UuidQuery'>) => {
-        const uuid = (await DatabaseLayer.makeRequest(
-          'UuidQuery',
-          payload
-        ))
+        const uuid = await DatabaseLayer.makeRequest('UuidQuery', payload)
         if (!isSupportedUuid(uuid)) return null
         if (uuid.__typename === DiscriminatorType.User) {
           const kratosIdentity =
