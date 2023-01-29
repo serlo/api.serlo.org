cfb5a992a9a4cadbbad984f80b4f01eda1811c75
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-01-29 23:54:51 +0100

    test(notification): add shorthand for EventQuery and complete refactoring

--- __tests__/__utils__/handlers.ts
+++ __tests__/__utils__/handlers.ts
@@ -42,6 +42,11 @@ const ForDefinitions = {
       given('UuidQuery').withPayload({ id: uuid.id }).returns(uuid)
     }
   },
+  EventQuery(events: Model<'AbstractNotificationEvent'>[]) {
+    events.map((event) => {
+      given('EventQuery').withPayload({ id: event.id }).returns(event)
+    })
+  },
   SubjectsQuery(terms: Model<'TaxonomyTerm'>[]) {
     given('SubjectsQuery')
       .withPayload({})
