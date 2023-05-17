356f0dd45bdce12cff4193c9321cd4c0311a4c18
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-05-17 14:27:59 +0200

    test(kratos): refactor to accomodate single logout endpoint tests

--- __tests__/internals/kratos-middleware.ts
+++ __tests__/internals/kratos-middleware.ts
@@ -31,36 +31,44 @@ const port = 8100
 let app: Express
 let server: Server
 
+interface IdentityApiGetIdentityRequest {
+  id: string
+}
+
+const kratosMock = {
+  admin: {
+    getIdentity: async (requestParameters: IdentityApiGetIdentityRequest) => {
+      return Promise.resolve({
+        data: {
+          id: requestParameters.id,
+          traits: {
+            username: 'user',
+            email: 'user@serlo.dev',
+          },
+        },
+      })
+    },
+    updateIdentity: async () => {
+      return Promise.resolve()
+    },
+    deleteIdentitySessions: async () => {
+      return Promise.resolve()
+    },
+  },
+  db: {
+    getIdByCredentialIdentifier: async () => {
+      return Promise.resolve('23af75f5-009a-4a11-a9d0-d79ac8bc8d34')
+    },
+  },
+} as unknown as Kratos
+
 beforeAll((done) => {
   app = express()
   app.use(express.json())
 
-  interface IdentityApiGetIdentityRequest {
-    id: string
-  }
-
   applyKratosMiddleware({
     app,
-    kratos: {
-      admin: {
-        getIdentity: async (
-          requestParameters: IdentityApiGetIdentityRequest
-        ) => {
-          return Promise.resolve({
-            data: {
-              id: requestParameters.id,
-              traits: {
-                username: 'user',
-                email: 'user@serlo.dev',
-              },
-            },
-          })
-        },
-        updateIdentity: async () => {
-          return Promise.resolve()
-        },
-      },
-    } as unknown as Kratos,
+    kratos: kratosMock,
   })
 
   server = app.listen(port, done)
@@ -72,14 +80,16 @@ afterAll((done) => {
 
 describe('Kratos middleware - register endpoint', () => {
   test('fails when secret is not sent', async () => {
-    const response = await fetchKratos({ withKratosKey: false })
+    const response = await fetchKratosRegister({ withKratosKey: false })
 
     expect(response.status).toBe(401)
     expect(await response.text()).toBe('Kratos secret mismatch')
   })
 
   test('fails when userId is not provided', async () => {
-    const response = await fetchKratos({ withKratosKey: true, body: {} })
+    const response = await fetchKratosRegister({
+      body: {},
+    })
 
     expect(response.status).toBe(400)
     expect(await response.text()).toBe('Valid identity id has to be provided')
@@ -87,8 +97,7 @@ describe('Kratos middleware - register endpoint', () => {
 
   test('fails when an invalid userId is provided', async () => {
     // kratos identity is a uuidv4, not a number like in legacy
-    const response = await fetchKratos({
-      withKratosKey: true,
+    const response = await fetchKratosRegister({
       body: { userId: 1 },
     })
 
@@ -102,8 +111,7 @@ describe('Kratos middleware - register endpoint', () => {
       success: true,
     })
 
-    const response = await fetchKratos({
-      withKratosKey: true,
+    const response = await fetchKratosRegister({
       body: { userId: 'e4c6a348-ab17-4b82-95e9-c5354a7f58eb' },
     })
 
@@ -115,18 +123,58 @@ describe('Kratos middleware - register endpoint', () => {
   })
 })
 
-function fetchKratos(args: { withKratosKey: boolean; body?: unknown }) {
-  const { withKratosKey, body } = args
+describe('Kratos middleware - single-logout endpoint', () => {
+  test('fails when payload has not logout_token in body', async () => {
+    kratosMock.db.getIdByCredentialIdentifier = async () => {
+      return Promise.resolve(null)
+    }
+    const response = await fetchKratosSingleLogout({
+      body: {},
+    })
+    expect(response.status).toBe(400)
+  })
+})
 
-  return fetch(`http://localhost:${port}/kratos/register`, {
+function fetchKratos(args: {
+  endpoint: string
+  withKratosKey: boolean
+  contentType: 'json' | 'x-www-form-urlencoded'
+  body?: unknown
+}) {
+  const { endpoint, withKratosKey, contentType, body } = args
+
+  return fetch(`http://localhost:${port}/kratos/${endpoint}`, {
     method: 'POST',
     headers: {
       'x-msw-bypass': 'true',
-      'content-type': 'application/json',
+      'content-type': `application/${contentType}`,
       ...(withKratosKey
         ? { 'x-kratos-key': process.env.SERVER_KRATOS_SECRET }
         : {}),
     },
     ...(body != null ? { body: JSON.stringify(body) } : {}),
+  })
+}
+
+function fetchKratosRegister({
+  withKratosKey = true,
+  body,
+}: {
+  withKratosKey?: boolean
+  body?: unknown
+}) {
+  return fetchKratos({
+    endpoint: 'register',
+    contentType: 'json',
+    ...{ withKratosKey, body },
+  })
+}
+
+function fetchKratosSingleLogout({ body }: { body?: unknown }) {
+  return fetchKratos({
+    endpoint: 'single-logout',
+    contentType: 'x-www-form-urlencoded',
+    withKratosKey: false,
+    body,
   })
 }
