0603d611f05371c141df747154c4294af9e21de3
Vitomir Budimir <budimirvitomir@gmail.com>
2023-09-25 13:03:58 +0200

    feat(content-generation): user input validation

--- packages/server/src/schema/content-generation/resolvers.ts
+++ packages/server/src/schema/content-generation/resolvers.ts
@@ -1,3 +1,6 @@
+import { either as E } from 'fp-ts'
+
+import * as ContentGenerationService from '../../model/content-generation'
 import { createNamespace, Queries } from '~/internals/graphql'
 
 export const resolvers: Queries<'contentGeneration'> = {
@@ -6,8 +9,29 @@ export const resolvers: Queries<'contentGeneration'> = {
   },
   ContentGenerationQuery: {
     async generatedContent(_parent, payload, { dataSources }) {
-      // @ts-expect-error TODO: Eslint complaining, no idea why
-      return await dataSources.model.serlo.getGeneratedContent(payload)
+      try {
+        const decodedUserInput =
+          ContentGenerationService.UserInputDecoder.decode(payload)
+
+        if (E.isLeft(decodedUserInput)) {
+          throw new Error('Invalid user input')
+        }
+
+        const userInput = decodedUserInput.right
+
+        const generatedContent =
+          await dataSources.model.serlo.getGeneratedContent(userInput)
+
+        return {
+          success: true,
+          generatedContent,
+        }
+      } catch (error) {
+        return {
+          success: false,
+          generatedContent: null,
+        }
+      }
     },
   },
 }
