d554b92c2173c3bdc71b575103aad2597ee5b8a2
Mikey Stengel <mikey@serlo.org>
2023-11-09 12:56:25 +0100

    Implement feedback of Kulla

--- packages/server/src/model/ai.ts
+++ packages/server/src/model/ai.ts
@@ -1,4 +1,4 @@
-import { option as O, function as F } from 'fp-ts'
+import { option as O, function as F, either as Either } from 'fp-ts'
 import * as t from 'io-ts'
 
 import { UserInputError } from '~/errors'
@@ -7,11 +7,19 @@ export const PayloadDecoder = t.strict({
   prompt: t.string,
 })
 
-type AnyJsonResponse = Record<string, unknown>
+const UnknownRecord = t.UnknownRecord
+
+type AnyJsonResponse = t.TypeOf<typeof UnknownRecord>
+
+export const isAnyJsonResponse = (
+  response: unknown,
+): response is AnyJsonResponse => {
+  return UnknownRecord.is(response)
+}
 
 export async function makeRequest({
   prompt,
-}: t.TypeOf<typeof PayloadDecoder>): Promise<Record<string, unknown>> {
+}: t.TypeOf<typeof PayloadDecoder>): Promise<AnyJsonResponse> {
   const url = new URL(
     `http://${process.env.CONTENT_GENERATION_SERVICE_HOST}/execute`,
   )
@@ -21,18 +29,23 @@ export async function makeRequest({
   const response = await fetch(url.href)
 
   if (response.status === 200) {
-    const responseJson = (await response.json()) as unknown as AnyJsonResponse
+    const responseJson = (await response.json()) as unknown
+
+    if (!isAnyJsonResponse(responseJson)) {
+      throw new Error('Invalid JSON format of content-generation-service')
+    }
+
     return responseJson
   } else if (response.status === 400) {
-    const responseJson = (await response.json()) as unknown as AnyJsonResponse
-    const reason = F.pipe(
-      O.tryCatch(() => responseJson as unknown),
-      O.chain(O.fromPredicate(t.type({ reason: t.string }).is)),
-      O.map((json) => json.reason),
-      O.getOrElse(() => 'Bad Request'),
-    )
+    const responseJson = (await response.json()) as unknown
+    const reasonDecoder = t.type({ reason: t.string })
+    const result = reasonDecoder.decode(responseJson)
 
-    throw new UserInputError(reason)
+    if (Either.isRight(result)) {
+      throw new UserInputError(result.right.reason)
+    } else {
+      throw new UserInputError('Bad Request')
+    }
   } else {
     throw new Error(`${response.status}`)
   }
