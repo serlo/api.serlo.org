b512ef910609c3b36bfe610ae79161c9fb3b8e08
Stephan Kulla <git.mail@kulla.me>
2023-12-06 21:24:11 +0100

    test: Use default-import only in CommonJS runtime mode

--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -7,3 +7,22 @@ export function isDefined<A>(value?: A | null): value is A {
 export function isDateString(text: string) {
   return !isNaN(new Date(text).getDate())
 }
+
+/**
+ * Some CommonJS libraries do not properly export a default value (see
+ * https://www.npmjs.com/package/default-import for an example). This function
+ * extracts the default value from such libraries only iff we are currently
+ * in ESM mode.
+ */
+export async function useDefaultImport<A>(value: A): Promise<A> {
+  // The function `require` does only exists in CommonJS mode
+  if (typeof require === 'function') return value
+
+  // Since `default-import` is a ESM only library do not import it statically
+  // because otherwise we would get an error in CommonJS runtime mode.
+  const { default: defaultImport } = await import('default-import')
+
+  // @ts-expect-error The types do not really match with the dynamic import ->
+  // Let's ignore this error for now...
+  return defaultImport(value) as A
+}
