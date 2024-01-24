88bb01a19cb33b5c963416375b6df65d330dd25b
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-01-25 00:12:11 +0100

    fix(mysql access): try returning {success: true}

--- packages/server/src/model/database.ts
+++ packages/server/src/model/database.ts
@@ -31,7 +31,7 @@ const runSql = async (
 export const setUserDescription = async (
   description: string,
   userId: number,
-): Promise<void> => {
+): Promise<string> => {
   if (description.length >= 64 * 1024) {
     throw new Error('Error setting description: too long')
   }
@@ -39,4 +39,5 @@ export const setUserDescription = async (
     description,
     userId,
   ])
+  return "{ success: true }"
 }
