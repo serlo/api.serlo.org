7a6aaa3b9db9dd5d81e15e9b51fd0d48c97af183
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-01-26 10:04:36 +0100

    refactor(mysql access): use only one environment variable with URI

--- packages/server/src/model/database.ts
+++ packages/server/src/model/database.ts
@@ -2,15 +2,7 @@ import * as mysql from 'mysql2/promise'
 
 import { log } from '../internals/log'
 
-const dbConfig = {
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB_NAME,
-  connectionLimit: 10,
-}
-
-const pool = mysql.createPool(dbConfig)
+const pool = mysql.createPool(process.env.MYSQL_URI)
 
 const runSql = async (
   query: string,
