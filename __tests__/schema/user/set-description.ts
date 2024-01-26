1e5a7aa5352eec6fbb99c3990fa02a532e1c1960
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-01-26 10:16:52 +0100

    docs(mysql access): change test case description to imply conditional relation instead of temporal

--- __tests__/schema/user/set-description.ts
+++ __tests__/schema/user/set-description.ts
@@ -21,7 +21,7 @@ beforeEach(() => {
   given('UuidQuery').for(user)
 })
 
-test('returns "{ success: true }" when mutation could be successfully executed', async () => {
+test('returns "{ success: true }" if mutation could be successfully executed', async () => {
   given('UserSetDescriptionMutation')
     .withPayload({ userId: user.id, description: 'description' })
     .returns({ success: true })
@@ -31,17 +31,17 @@ test('returns "{ success: true }" when mutation could be successfully executed',
   })
 })
 
-test('fails when user is not authenticated', async () => {
+test('fails if user is not authenticated', async () => {
   await mutation.forUnauthenticatedUser().shouldFailWithError('UNAUTHENTICATED')
 })
 
-test('fails when database layer returns a 400er response', async () => {
+test('fails if database layer returns a 400er response', async () => {
   given('UserSetDescriptionMutation').returnsBadRequest()
 
   await mutation.shouldFailWithError('BAD_USER_INPUT')
 })
 
-test('fails when database layer has an internal error', async () => {
+test('fails if database layer has an internal error', async () => {
   given('UserSetDescriptionMutation').hasInternalServerError()
 
   await mutation.shouldFailWithError('INTERNAL_SERVER_ERROR')
