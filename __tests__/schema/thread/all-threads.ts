7c6aaec56efb6ab44b06dc279a43b8b53be045a9
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-02-28 18:16:00 +0100

    test(all-threads): fix some tests by adjusting mocks

--- __tests__/schema/thread/all-threads.ts
+++ __tests__/schema/thread/all-threads.ts
@@ -2,19 +2,17 @@ import gql from 'graphql-tag'
 import * as R from 'ramda'
 
 import {
+  user,
+  user2,
   article,
   article2,
-  comment,
-  comment1,
-  comment2,
-  comment3,
 } from '../../../__fixtures__'
 import { Client, given, nextUuid } from '../../__utils__'
 import { Model } from '~/internals/graphql'
-import { castToUuid } from '~/model/decoder'
 import { encodeSubjectId } from '~/schema/subject/utils'
 import { encodeThreadId } from '~/schema/thread/utils'
 import { Instance } from '~/types'
+import { castToAlias, castToUuid, DiscriminatorType } from '~/model/decoder'
 
 function getThreadData(comment: Model<'Comment'>) {
   return {
@@ -22,20 +20,91 @@ function getThreadData(comment: Model<'Comment'>) {
     __typename: 'Thread',
     createdAt: comment.date,
   }
+}  
+
+export const comment: Model<'Comment'> = {
+  id: castToUuid(35163),
+  trashed: false,
+  alias: castToAlias('/mathe/27778/applets-vertauscht'),
+  __typename: DiscriminatorType.Comment,
+  authorId: user.id,
+  title: 'Falsche Lösungen',
+  date: '2015-02-21 13:13:24',
+  archived: false,
+  content:
+    'Der Autor selbst hat erkannt, dass man hier die Reihenfolge beachten muss dennoch benutzt er als Lösungsvorschlag die Kombination bei der die Reihenfolge natürlich missachtet bleibt.',
+  parentId: article.id,
+  childrenIds: [],
+  status: 'open',
+}
+
+export const comment1: Model<'Comment'> = {
+  id: castToUuid(35090),
+  trashed: false,
+  alias: castToAlias('/mathe/41443/related-content-ist-chaotisch'),
+  __typename: DiscriminatorType.Comment,
+  authorId: user.id,
+  parentId: article.id,
+  title: 'Hier fehlen uns noch Aufgaben',
+  date: '2015-02-19 16:47:16',
+  archived: false,
+  content:
+    'Kann jemand ein paar erstellen?',
+  childrenIds: [], //[49237].map(castToUuid),
+  status: 'done',
+}
+
+export const comment2: Model<'Comment'> = {
+  id: castToUuid(26976),
+  trashed: false,
+  alias: castToAlias('/mathe/49237/related-content'),
+  __typename: DiscriminatorType.Comment,
+  authorId: user2.id,
+  parentId: comment1.id,
+  title: 'related content aufräumen',
+  date: '2014-08-05 07:36:24',
+  archived: false,
+  content:
+    'Dieser Themenbaum ist etwas unübersichtlich, könnte man da die Größen und Einheiten vielleicht zusammennehmen, damit eine bessere Übersicht entsteht?',
+  childrenIds: [],
+  status: 'noStatus',
+}
+
+export const comment3: Model<'Comment'> = {
+  id: castToUuid(35082),
+  trashed: false,
+  alias: castToAlias('/mathe/27144/feedback-zu-dem-artikel-über-das-formular'),
+  __typename: DiscriminatorType.Comment,
+  authorId: 10,
+  title: 'Aufgaben noch umwortieren',
+  date: '2015-02-19 16:01:46',
+  archived: false,
+  content:
+    'Hier müssen die Aufgaben noch in die content-group umsortiert werden.',
+  parentId: article2.id,
+  childrenIds: [],
+  status: 'open',
+}
+
+export const comment4: Model<'Comment'> = {
+  id: castToUuid(34793),
+  trashed: false,
+  alias: castToAlias('/mathe/27144/feedback-zu-dem-artikel-über-das-formular'),
+  __typename: DiscriminatorType.Comment,
+  authorId: 10,
+  title: 'Verschiebung von Ordnern',
+  date: '2015-02-16 17:29:30',
+  archived: false,
+  content:
+    'Ist das nun so, wie du es meintest?',
+  parentId: article2.id,
+  childrenIds: [],
+  status: 'open',
 }
 
 describe('allThreads', () => {
-  const comment4 = { ...comment3, id: nextUuid(comment3.id) }
-  const firstThreads = [35163, 35090, 26976, 35082, 34793]
-
   beforeEach(() => {
-    given('UuidQuery').for(
-      { ...comment, id: castToUuid(firstThreads[0]) },
-      { ...comment1, id: castToUuid(firstThreads[1]) },
-      { ...comment2, id: castToUuid(firstThreads[2]) },
-      { ...comment3, id: castToUuid(firstThreads[3]) },
-      { ...comment4, id: castToUuid(firstThreads[4]) },
-    )
+    given('UuidQuery').for(comment, comment1, comment2, comment3, comment4)
     given('UuidQuery').for(article, article2)
     given('SubjectsQuery').returns({
       subjects: [
@@ -87,11 +156,7 @@ describe('allThreads', () => {
       .shouldReturnData({
         thread: {
           allThreads: {
-            nodes: [
-              { ...comment, id: castToUuid(firstThreads[0]) },
-              { ...comment1, id: castToUuid(firstThreads[1]) },
-              { ...comment2, id: castToUuid(firstThreads[2]) },
-            ].map(getThreadData),
+            nodes: [comment, comment1, comment2].map(getThreadData),
           },
         },
       })
@@ -125,14 +190,9 @@ describe('allThreads', () => {
   })
 
   test('parameter "subjectId"', async () => {
-    given('AllThreadsQuery')
-      .withPayload({ first: 11, subjectId: article.canonicalSubjectId! })
-      .returns({
-        firstCommentIds: [comment, comment1].map(R.prop('id')),
-      })
-
     await query
       .withVariables({
+        first: 2,
         subjectId: encodeSubjectId(article.canonicalSubjectId!),
       })
       .shouldReturnData({
@@ -143,15 +203,9 @@ describe('allThreads', () => {
   })
 
   test('parameter "status"', async () => {
-    given('AllThreadsQuery')
-      .withPayload({ first: 11, status: 'open' })
-      .returns({
-        firstCommentIds: [comment, comment1].map(R.prop('id')),
-      })
-
     await query.withVariables({ status: 'open' }).shouldReturnData({
       thread: {
-        allThreads: { nodes: [comment, comment1].map(getThreadData) },
+        allThreads: { nodes: [comment, comment3, comment4].map(getThreadData) },
       },
     })
   })
