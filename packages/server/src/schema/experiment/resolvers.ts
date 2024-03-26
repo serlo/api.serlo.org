7a79a60a028f06ce6f490062699a633f34e56f7f
shn-srl <shane.v.obrien@pm.me>
2024-03-26 12:44:18 +0100

    Update graphql types, add resolvers

--- packages/server/src/schema/experiment/resolvers.ts
+++ packages/server/src/schema/experiment/resolvers.ts
@@ -19,5 +19,58 @@ export const resolvers: Mutations<'experiment'> = {
       )
       return { success: true, query: {} }
     },
+    async createQuickbarStats(_parent, { input }) {
+      const { path, query, target, isSubject } = input
+      await runSql(
+        `
+        INSERT INTO 
+          test_survey 
+          (path, query, target, is_subject)
+        VALUES (?, ?, ?)
+        `,
+        [path, query, target, isSubject],
+      )
+      return { success: true, query: {} }
+    },
+    async createAbSubmission(_parent, { input }) {
+      const {
+        group,
+        experiment,
+        entityId,
+        type,
+        result,
+        sessionId,
+        isProduction,
+        topicId,
+      } = input
+      await runSql(
+        `
+        INSERT INTO ab_testing_data 
+          (experiment_group, experiment, entity_id, type, result, session_id, is_production, topic_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?);`,
+        [
+          group,
+          experiment,
+          entityId,
+          type,
+          result,
+          sessionId,
+          isProduction,
+          topicId,
+        ],
+      )
+      return { success: true, query: {} }
+    },
+    async createEquationsAppStats(_parent, { input }) {
+      const { event, latex, sessionId } = input
+      await runSql(
+        `
+          INSERT INTO equations_app_stats (event, latex, sessionI_id)
+          VALUES (?, ?, ?);
+        `,
+        [event, latex, sessionId],
+      )
+      return { success: true, query: {} }
+    },
   },
 }
