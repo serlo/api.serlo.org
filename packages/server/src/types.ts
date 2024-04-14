8129dbbd78c3d1f716bfcd89d6c7952a95dd7796
Stephan Kulla <git.mail@kulla.me>
2024-04-14 16:53:30 +0200

    refactor!: Remove Subject.subject()

--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -1942,13 +1942,7 @@ export type SubjectUnrevisedEntitiesArgs = {
 
 export type SubjectQuery = {
   __typename?: 'SubjectQuery';
-  subject?: Maybe<Subject>;
   subjects: Array<Subject>;
-};
-
-
-export type SubjectQuerySubjectArgs = {
-  id: Scalars['String']['input'];
 };
 
 
@@ -4137,7 +4131,6 @@ export type SubjectResolvers<ContextType = Context, ParentType extends Resolvers
 };
 
 export type SubjectQueryResolvers<ContextType = Context, ParentType extends ResolversParentTypes['SubjectQuery'] = ResolversParentTypes['SubjectQuery']> = {
-  subject?: Resolver<Maybe<ResolversTypes['Subject']>, ParentType, ContextType, RequireFields<SubjectQuerySubjectArgs, 'id'>>;
   subjects?: Resolver<Array<ResolversTypes['Subject']>, ParentType, ContextType, RequireFields<SubjectQuerySubjectsArgs, 'instance'>>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
