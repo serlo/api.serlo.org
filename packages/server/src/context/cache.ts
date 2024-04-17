4b4ad048c2bdb01fe9ae27df144210740de9ce68
Stephan Kulla <git.mail@kulla.me>
2024-04-17 15:22:03 +0200

    yarn format:prettier

--- packages/server/src/context/cache.ts
+++ packages/server/src/context/cache.ts
@@ -1,31 +1,30 @@
-import { option as O } from "fp-ts";
+import { option as O } from 'fp-ts'
 
-import { Time } from '~/timer';
-import { FunctionOrValue } from '~/utils';
+import { Time } from '~/timer'
+import { FunctionOrValue } from '~/utils'
 
 export interface Cache {
-  get<T>(args: { key: string; maxAge?: Time; }): Promise<O.Option<CacheEntry<T>>>;
+  get<T>(args: { key: string; maxAge?: Time }): Promise<O.Option<CacheEntry<T>>>
   set<T>(
     payload: {
-      key: string;
-      source: string;
-      ttlInSeconds?: number;
-      priority?: Priority;
-    } & FunctionOrValue<T>
-  ): Promise<void>;
-  remove(args: { key: string; }): Promise<void>;
-  ready(): Promise<void>;
-  flush(): Promise<void>;
-  quit(): Promise<void>;
+      key: string
+      source: string
+      ttlInSeconds?: number
+      priority?: Priority
+    } & FunctionOrValue<T>,
+  ): Promise<void>
+  remove(args: { key: string }): Promise<void>
+  ready(): Promise<void>
+  flush(): Promise<void>
+  quit(): Promise<void>
 }
 
 export enum Priority {
   Low,
-  High
+  High,
 }
 export interface CacheEntry<Value> {
   value: Value
   lastModified: number
   source: string
 }
-
--- packages/server/src/timer.ts
+++ packages/server/src/timer.ts
@@ -8,18 +8,21 @@ export function createTimer(): Timer {
       return Date.now()
     },
   }
-}export interface Time {
+}
+export interface Time {
   days?: number
   hours?: number
   minutes?: number
   seconds?: number
 }
 export function timeToSeconds({
-  days = 0, hours = 0, minutes = 0, seconds = 0,
+  days = 0,
+  hours = 0,
+  minutes = 0,
+  seconds = 0,
 }: Time) {
   return ((days * 24 + hours) * 60 + minutes) * 60 + seconds
 }
 export function timeToMilliseconds(time: Time) {
   return timeToSeconds(time) * 1000
 }
-
