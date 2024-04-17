ae64e6f130940b73228ef12d955d187ece3144c4
Stephan Kulla <git.mail@kulla.me>
2024-04-17 15:21:20 +0200

    refactor: Move "Service" to ~/context

--- packages/server/src/context/index.ts
+++ packages/server/src/context/index.ts
@@ -1,16 +1,15 @@
 import { type Storage } from '@google-cloud/storage'
 
-// import { type Service } from '../internals/authentication'
 // import { type ModelDataSource } from '../internals/data-source'
 // import { type SwrQueue } from '../internals/swr-queue'
 import { type Cache } from './cache'
 import { type Database } from '~/database'
+import { Service } from './service'
 
 export interface Context {
   // dataSources: {
   //  model: ModelDataSource
   //}
-  // service: Service
   userId: number | null
   googleStorage: Storage
   database: Database
@@ -18,5 +17,5 @@ export interface Context {
   dataSources: any
   cache: Cache
   swrQueue: any
-  service: any
+  service: Service
 }
