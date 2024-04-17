15d3ca3f1efbda822d77beae9151ef1d80ac85f9
Stephan Kulla <git.mail@kulla.me>
2024-04-17 15:52:17 +0200

    refactor: Reenable DataSource

--- packages/server/src/context/index.ts
+++ packages/server/src/context/index.ts
@@ -1,20 +1,18 @@
 import { type Storage } from '@google-cloud/storage'
 
-// import { type ModelDataSource } from '../internals/data-source'
+import { type ModelDataSource } from '../internals/data-source'
 import { type Cache } from './cache'
 import { type Database } from '~/database'
 import { Service } from './service'
 import { SwrQueue } from './swr-queue'
 
 export interface Context {
-  // dataSources: {
-  //  model: ModelDataSource
-  //}
+   dataSources: {
+    model: ModelDataSource
+  }
   userId: number | null
   googleStorage: Storage
   database: Database
-  // swrQueue: SwrQueue
-  dataSources: any
   cache: Cache
   swrQueue: SwrQueue
   service: Service
