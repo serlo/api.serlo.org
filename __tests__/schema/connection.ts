e7ae3734da061af83b1585f037e01daeaba99e26
Stephan Kulla <git.mail@kulla.me>
2024-04-18 14:20:24 +0200

    refactor: Simplify resolveConnection()

--- __tests__/schema/connection.ts
+++ __tests__/schema/connection.ts
@@ -24,13 +24,10 @@ describe('resolveConnection', () => {
 
   test('without payload', () => {
     expect(getResult({})).toEqual({
-      edges,
       nodes,
       totalCount: 9,
       pageInfo: {
         hasNextPage: false,
-        hasPreviousPage: false,
-        startCursor: edges[0].cursor,
         endCursor: edges[8].cursor,
       },
     })
@@ -39,25 +36,19 @@ describe('resolveConnection', () => {
   test('forward pagination (by 5)', () => {
     const page1 = getResult({ first: 5 })
     expect(page1).toEqual({
-      edges: edges.slice(0, 5),
       nodes: nodes.slice(0, 5),
       totalCount: 9,
       pageInfo: {
         hasNextPage: true,
-        hasPreviousPage: false,
-        startCursor: edges[0].cursor,
         endCursor: edges[4].cursor,
       },
     })
     const page2 = getResult({ first: 5, after: page1.pageInfo.endCursor! })
     expect(page2).toEqual({
-      edges: edges.slice(5, 9),
       nodes: nodes.slice(5, 9),
       totalCount: 9,
       pageInfo: {
         hasNextPage: false,
-        hasPreviousPage: true,
-        startCursor: edges[5].cursor,
         endCursor: edges[8].cursor,
       },
     })
@@ -75,37 +66,13 @@ test('sets "first = limit" when first and limit are not defined', () => {
   expect(result.nodes.length).toBe(500)
 })
 
-describe('throws an error', () => {
-  test('when first > limit', () => {
-    expect(() => {
-      resolveConnection({
-        nodes: R.range(1, 2000),
-        payload: { first: 1000 },
-        createCursor: (node) => node.toString(),
-        limit: 500,
-      })
-    }).toThrowError(UserInputError)
-  })
-
-  test('when last > limit', () => {
-    expect(() => {
-      resolveConnection({
-        nodes: R.range(1, 2000),
-        payload: { last: 1000 },
-        createCursor: (node) => node.toString(),
-        limit: 500,
-      })
-    }).toThrowError(UserInputError)
-  })
-
-  test('when first and last is set (because then the behavior is ambiguous)', () => {
-    expect(() => {
-      resolveConnection({
-        nodes: R.range(1, 2000),
-        payload: { last: 10, first: 10 },
-        createCursor: (node) => node.toString(),
-        limit: 500,
-      })
-    }).toThrowError(UserInputError)
-  })
+test('throws an error when first > limit', () => {
+  expect(() => {
+    resolveConnection({
+      nodes: R.range(1, 2000),
+      payload: { first: 1000 },
+      createCursor: (node) => node.toString(),
+      limit: 500,
+    })
+  }).toThrowError(UserInputError)
 })
