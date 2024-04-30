7db41a7aba7ab9acdeab5d255e6dcaa9bd442c26
Stephan Kulla <git.mail@kulla.me>
2024-04-30 18:19:24 +0200

    fix(event): Fix conversion ConcreteEvent <=> AbstractEvent

--- __tests__/internals/create-event.ts
+++ __tests__/internals/create-event.ts
@@ -5,7 +5,29 @@
  * It will be better to use the createEvent function in a real
  * world test case, and then remove this file.
  */
-import { article, comment, user } from '../../__fixtures__'
+import * as R from 'ramda'
+
+import {
+  article,
+  comment,
+  user,
+  checkoutRevisionNotificationEvent,
+  createCommentNotificationEvent,
+  createEntityLinkNotificationEvent,
+  createEntityNotificationEvent,
+  createEntityRevisionNotificationEvent,
+  createTaxonomyLinkNotificationEvent,
+  createTaxonomyTermNotificationEvent,
+  createThreadNotificationEvent,
+  rejectRevisionNotificationEvent,
+  removeEntityLinkNotificationEvent,
+  removeTaxonomyLinkNotificationEvent,
+  setLicenseNotificationEvent,
+  setTaxonomyParentNotificationEvent,
+  setTaxonomyTermNotificationEvent,
+  setThreadStateNotificationEvent,
+  setUuidStateNotificationEvent,
+} from '../../__fixtures__'
 import { NotificationEventType } from '~/model/decoder'
 import {
   createEvent,
@@ -21,6 +43,87 @@ const basePayload = {
   threadId: article.id,
   commentId: comment.id,
 } as const
+
+describe('creates event successfully with right payload', () => {
+  const events = [
+    checkoutRevisionNotificationEvent,
+    createCommentNotificationEvent,
+    createEntityLinkNotificationEvent,
+    createEntityNotificationEvent,
+    createEntityRevisionNotificationEvent,
+    createTaxonomyLinkNotificationEvent,
+    createTaxonomyTermNotificationEvent,
+    createThreadNotificationEvent,
+    rejectRevisionNotificationEvent,
+    removeEntityLinkNotificationEvent,
+    removeTaxonomyLinkNotificationEvent,
+    setLicenseNotificationEvent,
+    setTaxonomyParentNotificationEvent,
+    setTaxonomyTermNotificationEvent,
+    setThreadStateNotificationEvent,
+    setUuidStateNotificationEvent,
+  ] as const
+
+  const testCases: [string, (typeof events)[number]][] = events.map((event) => [
+    event.__typename,
+    event,
+  ])
+
+  test.each(testCases)('%s', async (_, event) => {
+    const initialEventsNumber = await getEventsNumber()
+
+    await createEvent(event, getContext())
+
+    const lastAbstractEvent = await database.fetchOne<AbstractEvent>(`
+      select
+        event_log.id as id,
+        event.name as type,
+        event_log.actor_id as actorId,
+        instance.subdomain as instance,
+        event_log.date as date,
+        event_log.uuid_id as objectId,
+        JSON_OBJECTAGG(
+          COALESCE(event_parameter_name.name, "__unused"),
+          event_parameter_uuid.uuid_id
+        ) as uuidParameters,
+        JSON_OBJECTAGG(
+          COALESCE(event_parameter_name.name, "__unused"),
+          event_parameter_string.value
+        ) as stringParameters
+      from event_log
+      join event on event.id = event_log.event_id
+      join instance on event_log.instance_id = instance.id
+      left join event_parameter on event_parameter.log_id = event_log.id
+      left join event_parameter_name on event_parameter.name_id = event_parameter_name.id
+      left join event_parameter_string on event_parameter_string.event_parameter_id = event_parameter.id
+      left join event_parameter_uuid on event_parameter_uuid.event_parameter_id = event_parameter.id
+      group by event_log.id
+      order by id desc
+      limit 1
+    `)
+
+    const lastEvent = toConcreteEvent(lastAbstractEvent)
+
+    expect(lastEvent).toEqual({
+      id: expect.any(Number) as number,
+      date: expect.any(String) as string,
+      ...R.omit(['id', 'date'], event),
+    })
+
+    /*
+    expect(
+      await database.fetchAll(
+        'select * from notification_event where event_log_id = ?',
+        [event.id],
+      ),
+    ).not.toHaveLength(0)
+    // TODO: be sure the notifications are set to right users and objects
+    */
+
+    const finalEventsNumber = await getEventsNumber()
+    expect(finalEventsNumber).not.toEqual(initialEventsNumber)
+  })
+})
 
 test('fails if actor does not exist', async () => {
   await expect(
@@ -51,58 +154,6 @@ test('fails if uuid number in parameters does not exist', async () => {
   await expect(
     createEvent({ ...basePayload, threadId: 40000 }, getContext()),
   ).rejects.toThrow()
-})
-
-test('creates event successfully with right payload', async () => {
-  const initialEventsNumber = await getEventsNumber()
-
-  await createEvent(basePayload, getContext())
-
-  const lastAbstractEvent = await database.fetchOne<AbstractEvent>(`
-      select
-        event_log.id as id,
-        event.name as type,
-        event_log.actor_id as actorId,
-        instance.subdomain as instance,
-        event_log.date as date,
-        event_log.uuid_id as objectId,
-        JSON_OBJECTAGG(event_parameter_name.name, event_parameter_uuid.uuid_id) as uuidParameters,
-        JSON_OBJECTAGG(event_parameter_name.name, event_parameter_string.value) as stringParameters
-      from event_log
-      join event on event.id = event_log.event_id
-      join instance on event_log.instance_id = instance.id
-      join event_parameter on event_parameter.log_id = event_log.id
-      join event_parameter_name on event_parameter.name_id = event_parameter_name.id
-      left join event_parameter_string on event_parameter_string.event_parameter_id = event_parameter.id
-      left join event_parameter_uuid on event_parameter_uuid.event_parameter_id = event_parameter.id
-      group by event_log.id
-      order by id desc
-      limit 1
-    `)
-
-  const event = toConcreteEvent(lastAbstractEvent)
-
-  expect(event).toEqual({
-    id: expect.any(Number) as number,
-    date: expect.any(String) as string,
-    actorId: basePayload.actorId,
-    instance: basePayload.instance,
-    objectId: basePayload.commentId,
-    __typename: basePayload.__typename,
-    threadId: basePayload.threadId,
-    commentId: basePayload.commentId,
-  })
-
-  expect(
-    await database.fetchAll(
-      'select * from notification_event where event_log_id = ?',
-      [event.id],
-    ),
-  ).not.toHaveLength(0)
-  // TODO: be sure the notifications are set to right users and objects
-
-  const finalEventsNumber = await getEventsNumber()
-  expect(finalEventsNumber).not.toEqual(initialEventsNumber)
 })
 
 async function getEventsNumber() {
