7a4186f4cab3b2152ba20733ed76e83e67e4b41f
Stephan Kulla <git.mail@kulla.me>
2024-05-05 16:53:58 +0200

    test: Move taxonomyTermQuery to __utils__
    When a test file imports another one their tests are also executed when
    the file is called => So we move it to `__tests__/__utils__/`

--- __tests__/__utils__/index.ts
+++ __tests__/__utils__/index.ts
@@ -3,6 +3,7 @@ import * as R from 'ramda'
 export * from './assertions'
 export * from './handlers'
 export * from './services'
+export * from './query'
 
 export function getTypenameAndId(value: { __typename: string; id: number }) {
   return R.pick(['__typename', 'id'], value)
