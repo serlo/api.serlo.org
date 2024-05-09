bf72838afdf04e4f13ed2be58d97a09b6d079b1e
Stephan Kulla <git.mail@kulla.me>
2024-05-09 20:21:39 +0200

    test: Remove given() of subject.ts

--- __tests__/schema/subject.ts
+++ __tests__/schema/subject.ts
@@ -1,7 +1,6 @@
 import gql from 'graphql-tag'
 
-import { article, taxonomyTermSubject } from '../../__fixtures__'
-import { Client, given, subjectQuery } from '../__utils__'
+import { Client, subjectQuery } from '../__utils__'
 
 test('endpoint "subjects" returns list of all subjects for an instance', async () => {
   await subjectQuery.withVariables({ instance: 'en' }).shouldReturnData({
@@ -16,8 +15,6 @@ test('`Subject.id` returns encoded id of subject', async () => {
 })
 
 test('`Subject.unrevisedEntities` returns list of unrevisedEntities', async () => {
-  given('UuidQuery').for(taxonomyTermSubject, article)
-
   await new Client()
     .prepareQuery({
       query: gql`
