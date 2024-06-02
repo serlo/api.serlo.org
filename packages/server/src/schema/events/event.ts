c22062c85c68e46a766d12ab15cadcf656bc63ba
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-06-02 21:44:53 +0200

    Revert "fix: Use correct timestamp at notifications"

--- packages/server/src/schema/events/event.ts
+++ packages/server/src/schema/events/event.ts
@@ -142,15 +142,9 @@ async function createNotifications(
 export function toGraphQLModel(
   event: DatabaseEventRepresentation,
 ): GraphQLEventModels {
-  // Due to old hack Germany dates are saved in database as UTC.
-  // To output them we correct them back as real UTC.
-  const clonedDate = new Date(event.date)
-  const hackedDate = new Date(
-    clonedDate.setMinutes(event.date.getTimezoneOffset()),
-  )
   const base = {
     ...R.pick(['id', 'actorId', 'instance', 'objectId'], event),
-    date: hackedDate.toISOString(),
+    date: event.date.toISOString(),
   }
 
   if (
