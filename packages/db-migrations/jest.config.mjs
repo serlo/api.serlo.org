017ce4315c37deb8ea9c1e4f192521cae2076608
Hugo Tiburtino <hugotiburtino@protonmail.com>
2024-06-09 18:19:17 +0200

    refactor(db-migration): remove unnecessary now things

--- packages/db-migrations/jest.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true, isolatedModules: true }],
-  },
-  extensionsToTreatAsEsm: ['.ts', '.tsx', '.mts'],
-  setupFiles: ['dotenv/config'],
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/'],
-}
