b79e8c084cf05510581d35dcde5065e3cb25fd86
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-08-06 09:55:47 +0200

    style: fix import order

--- __tests__/schema/uuid/user.ts
+++ __tests__/schema/uuid/user.ts
@@ -16,8 +16,8 @@ import {
   createFakeIdentity,
 } from '../../__utils__'
 import { Model } from '~/internals/graphql'
-import { Instance } from '~/types'
 import { MajorDimension } from '~/model'
+import { Instance } from '~/types'
 
 const client = new Client()
 const adminUserId = 1
