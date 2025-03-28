391d07b93f3d82b86a11058383eb9cff9db5ff69
hugotiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-03-27 22:13:40 -0300

    Add instructions for set staging db up

--- deploy/README.md
+++ deploy/README.md
@@ -1,19 +1,25 @@
 # Deployment Instructions with Docker Compose
 
 ## Requirements
+
 - Docker
 - Nginx
 - Git
+- GCloud CLI
+- Gsutil
 
 ## Steps for the Staging deployment
 
 1. `git clone https://github.com/serlo/api.serlo.org && cd api.serlo.org`
 2. Set up Nginx on the host machine using configuration file `nginx.staging.conf`.
+
 ```console
 $ sudo cp nginx.staging.conf /etc/nginx/sites-available/default
 $ sudo systemctl restart nginx
 ```
+
 3. Remember to set up SSL certificates (currently, self-signed ones are enough):
+
 ```
 $ sudo mkdir -p /etc/nginx/ssl
 % sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
@@ -21,8 +27,18 @@ $ sudo mkdir -p /etc/nginx/ssl
   -out /etc/nginx/ssl/selfsigned.crt \
   -subj "/CN=*.serlo-staging.dev"
 ```
+
 4. Be sure the values at `.staging.env` and `kratos/config.staging.yml` (change the values with "PLACEHODER") are correct.
 5. Deploy using Docker Compose with file `docker-compose.staging.yml`.
+
 ```console
 $ docker compose -f docker-compose.staging.yml up -d
 ```
+
+6. Set up the Gsutil. You need to authenticate and may use a key of the appropriate service account
+   1. Go to GC Console -> IAM -> Service Accounts -> choose the dbreader account -> generate a new one
+   2. Put the key in a file `staging_service_account_key.json` in the home directory
+   3. `echo $GCLOUD_SERVICE_ACCOUNT_KEY > $HOME/staging_service_account_key.json`
+   4. `gcloud auth activate-service-account ${GCLOUD_SERVICE_ACCOUNT_NAME} --key-file /tmp/service_account_key.json` Replace GCLOUD_SERVICE_ACCOUNT_NAME with the email of the service account.
+   5. Run `./dbsetup.sh`
+   6. Set cron tab to run the dbsetup script every night at 2 am.
---
+++ deploy/dbsetup.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+set -e
+
+mysql_connect="docker compose -f docker-compose.staging.yml exec mysql mysql --user=serlo --password=secret"
+
+echo "wait for mysql database to be ready"
+until $mysql_connect -e "SHOW DATABASES" >/dev/null 2>/dev/null; do
+    echo "could not find mysql server - retry in 10 seconds"
+    sleep 10
+done
+
+newest_dump_uri=$(gsutil ls -l gs://anonymous-dump | grep dump | sort -rk 2 | head -n 1 | awk '{ print $3 }')
+[ -z "$newest_dump_uri" ] && {
+    echo "no database dump available in anonymous db dump bucket"
+    exit 1
+}
+
+newest_dump=$(basename $newest_dump_uri)
+[ -f "/tmp/$newest_dump" ] && exit 0
+
+gsutil cp $newest_dump_uri "/tmp/$newest_dump"
+echo "downloaded newest dump $newest_dump"
+unzip -o "/tmp/$newest_dump" -d /tmp || {
+    echo "unzip of dump file failed"
+    exit 1
+}
+
+echo "Recreating serlo database"
+
+docker compose -f docker-compose.staging.yml cp /tmp/mysql.sql mysql:/tmp/mysql.sql
+docker compose -f docker-compose.staging.yml cp /tmp/user.csv mysql:/tmp/user.csv
+
+mysql $mysql_connect -e "DROP DATABASE serlo"
+mysql $mysql_connect -e "CREATE DATABASE serlo"
+mysql $mysql_connect serlo <"/tmp/mysql.sql" || {
+    echo "import of dump failed"
+    exit 1
+}
+mysql $mysql_connect -e "LOAD DATA LOCAL INFILE '/tmp/user.csv' INTO TABLE user FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" serlo || {
+    echo "import of dump failed"
+    exit 1
+}
+mysql $mysql_connect serlo -e "UPDATE user SET description = NULL WHERE description = 'NULL'"
+
+echo "imported serlo database dump $newest_dump"
+
+echo "Recreating kratos database"
+
+docker compose -f docker-compose.staging.yml cp /tmp/kratos.sql postgres:/tmp/kratos.sql
+
+postgres_connect="docker compose -f docker-compose.staging.yml exec postgres psql --user=serlo kratos "
+psql $postgres_connect -c "DROP SCHEMA public CASCADE;"
+psql $postgres_connect -c "CREATE SCHEMA public;"
+psql $postgres_connect -c "GRANT ALL ON SCHEMA public TO serlo;"
+psql $postgres_connect </tmp/kratos.sql
+
+echo "Recreated kratos database"
+
+echo "Deleting unnecessary files"
+rm -f $(ls /tmp/dump*.zip | grep -v $newest_dump)
+rm /tmp/*.sql /tmp/user.csv
