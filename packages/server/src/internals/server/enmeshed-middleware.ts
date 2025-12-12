15250d0e22c507ce64d0f50af352837435a06e0f
Stephan Kulla <git.mail@kulla.me>
2025-12-12 11:45:40 +0100

    fix: Add "bodyFormat"

--- packages/server/src/internals/server/enmeshed-middleware.ts
+++ packages/server/src/internals/server/enmeshed-middleware.ts
@@ -374,6 +374,7 @@ function createSetAttributesHandler(
         to: [session.enmeshedId],
         subject: 'Aktualisierung deines Lernstands',
         body: 'Gratulation!\nDu hast den Kurs zum logistischen Wachstum erfolgreich absolviert. Bitte speichere den aktualisierten Lernstand.\nDein Serlo-Team',
+        bodyFormat: 'PlainText',
       },
     })
 
